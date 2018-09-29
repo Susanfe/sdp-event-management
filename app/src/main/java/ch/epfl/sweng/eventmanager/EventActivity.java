a81762100f5d83e81a0ce39efe35c576792921dd
stanislas jouven <stanislas.jouven@flaminem.com>
2018-09-29 17:11:56 +0200

    add 3 activities map, schedule and ticketActivity and link buttons to activities

--- app/src/main/java/ch/epfl/sweng/eventmanager/EventActivity.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/EventActivity.java
@@ -1,7 +1,9 @@
 package ch.epfl.sweng.eventmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EventActivity extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class EventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
     }
+
+    public void goToMap(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSchedule(View view) {
+        Intent intent = new Intent(this, ScheduleActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToBuyingTicket(View view) {
+        Intent intent = new Intent(this, TicketActivity.class);
+        startActivity(intent);
+    }
+
 }
