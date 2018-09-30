a992b48ff7575f7462ecd7920af4f18fe0df1b44
Timothée Floure <timothee.floure@epfl.ch>
2018-09-30 16:49:51 +0200

    Event list: add padding and a selection button to the items

--- app/src/main/java/ch/epfl/sweng/eventmanager/EventListAdapter.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/EventListAdapter.java
@@ -2,52 +2,57 @@ package ch.epfl.sweng.eventmanager;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.MyViewHolder> {
-    private String[] mDataset;
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder> {
+    private String[] mEvents;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public MyViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
+        public TextView eventNameTextView;
+        public Button selectEventButton;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            eventNameTextView = (TextView) itemView.findViewById(R.id.event_name);
+            selectEventButton = (Button) itemView.findViewById(R.id.select_event_button);
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public EventListAdapter(String[] myDataset) {
-        mDataset = myDataset;
+    public EventListAdapter(String[] myEvents) {
+        mEvents = myEvents;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public EventListAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+    public EventListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        View v = (View) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.event_list_item, parent, false);
-        MyViewHolder vh = new MyViewHolder(v);
+        ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
+        holder.eventNameTextView.setText(mEvents[position]);
+        holder.selectEventButton.setText("Select »");
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mEvents.length;
     }
 }
