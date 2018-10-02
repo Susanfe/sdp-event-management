f097978b81ac5181c3bf74cde79d25e645195cac
Timothée Floure <timothee.floure@epfl.ch>
2018-10-02 11:41:53 +0200

    Re-import small file lost due to human error in last rebase

---
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/MapActivity.java
@@ -0,0 +1,15 @@
+package ch.epfl.sweng.eventmanager.ui.eventShowcase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.epfl.sweng.eventmanager.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
