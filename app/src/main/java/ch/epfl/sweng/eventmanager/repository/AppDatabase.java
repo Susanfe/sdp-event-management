54f37ff0d4e82850b24ce0ada1e41badeef3cb4c
AntoineCrettenand <lugiar13@gmail.com>
2018-10-06 16:25:55 +0200

    Move to repository

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/AppDatabase.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/AppDatabase.java
@@ -1,6 +1,8 @@
-package ch.epfl.sweng.eventmanager.repository.database;
+package ch.epfl.sweng.eventmanager.repository;
 
 import android.arch.persistence.room.Database;
+import ch.epfl.sweng.eventmanager.repository.database.Event;
+import ch.epfl.sweng.eventmanager.repository.database.EventDao;
 
 @Database(entities = {Event.class}, version = 1)
 public abstract class AppDatabase {
