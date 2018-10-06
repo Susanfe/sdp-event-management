373a6a4d6d27a4b00d77ceac16e285108a37c4ed
AntoineCrettenand <lugiar13@gmail.com>
2018-10-06 18:10:26 +0200

    Fix dagger2 inject with room

---
+++ app/src/main/java/ch/epfl/sweng/eventmanager/room/JoinedEventRepository.java
@@ -0,0 +1,44 @@
+package ch.epfl.sweng.eventmanager.room;
+
+import android.arch.lifecycle.LiveData;
+import ch.epfl.sweng.eventmanager.room.data.JoinedEvent;
+import ch.epfl.sweng.eventmanager.room.data.JoinedEventDao;
+
+import java.util.List;
+
+public class JoinedEventRepository {
+
+    private JoinedEventDao joinedEventDao;
+
+    public JoinedEventRepository(JoinedEventDao joinedEventDao){
+        this.joinedEventDao = joinedEventDao;
+    }
+
+    public LiveData<List<JoinedEvent>> findAll(){
+        return joinedEventDao.getAll();
+    }
+
+    public LiveData<JoinedEvent> findById(int eventId){
+        return joinedEventDao.findById(eventId);
+    }
+
+    public LiveData<JoinedEvent> findByName(String name){
+        return joinedEventDao.findByName(name);
+    }
+
+    public long insert(JoinedEvent joinedEvent){
+        return joinedEventDao.insert(joinedEvent);
+    }
+
+    public long insertAll(JoinedEvent... joinedEvents){
+        return joinedEventDao.insertAll(joinedEvents);
+    }
+
+    public int delete(JoinedEvent joinedEvent){
+        return joinedEventDao.delete(joinedEvent);
+    }
+
+    public int deleteAll(JoinedEvent... joinedEvents){
+        return joinedEventDao.deleteAll(joinedEvents);
+    }
+}
