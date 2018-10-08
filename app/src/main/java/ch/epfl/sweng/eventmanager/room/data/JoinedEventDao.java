3faa00acc66e1ba1c2dfe6b1d2491fc30260a163
AntoineCrettenand <lugiar13@gmail.com>
2018-10-08 16:49:01 +0200

    Delete unused queries

--- app/src/main/java/ch/epfl/sweng/eventmanager/room/data/JoinedEventDao.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/room/data/JoinedEventDao.java
@@ -7,26 +7,20 @@ import java.util.List;
 
 @Dao
 public interface JoinedEventDao {
-    @Query("SELECT * FROM `joined-events`")
+    @Query("SELECT * FROM joined_events")
     LiveData<List<JoinedEvent>> getAll();
 
-    @Query("SELECT * FROM `joined-events` WHERE `event-id` IN (:eventIds)")
+    @Query("SELECT * FROM joined_events WHERE event_id IN (:eventIds)")
     LiveData<List<JoinedEvent>> loadAllByIds(int[] eventIds);
 
-    @Query("SELECT * FROM `joined-events` WHERE name LIKE :eventName LIMIT 1")
+    @Query("SELECT * FROM joined_events WHERE name LIKE :eventName LIMIT 1")
     LiveData<JoinedEvent> findByName(String eventName);
 
-    @Query("SELECT * FROM `joined-events` WHERE `event-id` LIKE :eventId LIMIT 1")
+    @Query("SELECT * FROM joined_events WHERE event_id LIKE :eventId LIMIT 1")
     LiveData<JoinedEvent> findById(int eventId);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertAll(JoinedEvent... joinedEvents);
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(JoinedEvent joinedEvent);
-
-    @Delete
-    int deleteAll(JoinedEvent... joinedEvents);
 
     @Delete
     int delete(JoinedEvent joinedEvent);
