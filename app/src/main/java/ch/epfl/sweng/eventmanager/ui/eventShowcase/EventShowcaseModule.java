0f1c26d6aaa4ecc694280688c6473c91875f2298
zyuiop <zyuiop@zyuiop.net>
2018-10-08 11:53:17 +0200

    Remove subComponents (became useless)

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/EventShowcaseModule.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/EventShowcaseModule.java
@@ -1,21 +1,18 @@
 package ch.epfl.sweng.eventmanager.ui.eventShowcase;
 
-import android.app.Activity;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import ch.epfl.sweng.eventmanager.viewmodel.ViewModelFactory;
 import ch.epfl.sweng.eventmanager.viewmodel.ViewModelKey;
 import dagger.Binds;
 import dagger.Module;
-import dagger.android.ActivityKey;
-import dagger.android.AndroidInjector;
 import dagger.android.ContributesAndroidInjector;
 import dagger.multibindings.IntoMap;
 
 /**
  * @author Louis Vialar
  */
-@Module(subcomponents = {EventActivitySubcomponent.class})
+@Module
 public abstract class EventShowcaseModule {
     @Binds
     abstract ViewModelProvider.Factory bindViewModelFactory(ViewModelFactory factory);
@@ -25,18 +22,14 @@ public abstract class EventShowcaseModule {
     @ViewModelKey(EventShowcaseModel.class)
     abstract ViewModel provideEventListModel(EventShowcaseModel eventListModel);
 
-
-    @Binds
-    @IntoMap
-    @ActivityKey(EventActivity.class)
-    abstract AndroidInjector.Factory<? extends Activity>
-    bindEventActivityInjectorFactory(EventActivitySubcomponent.Builder builder);
-
     @ContributesAndroidInjector
     abstract MapActivity contributeMapActivityInjector();
 
     @ContributesAndroidInjector
     abstract ScheduleActivity contributeScheduleActivityInjector();
+
+    @ContributesAndroidInjector
+    abstract EventActivity contributeEventActivityInjector();
 
     @ContributesAndroidInjector
     abstract TicketActivity contributeTicketActivityInjector();
