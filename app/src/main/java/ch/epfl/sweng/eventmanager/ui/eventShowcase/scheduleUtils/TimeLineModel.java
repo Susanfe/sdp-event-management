c4e77d273217489de5c3e0c3933450d779b95d1d
Nicolas d'Argenlieu <nicolas.dargenlieu@epfl.ch>
2018-10-09 15:23:37 +0200

    Added isNow var and handled its initializiation
    isNow describes the status of the concert, it is -1 if the concert
    already happened, 0 if it is happening and 1 if it is going to happen.
    Handled initialization and usage for timelineView

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/scheduleUtils/TimeLineModel.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/scheduleUtils/TimeLineModel.java
@@ -8,22 +8,28 @@ import java.util.Date;
 public class TimeLineModel implements Parcelable {
 
     private String artist;
-    private String date;
+    private Date date;
+    private String stringDate;
     private String genre;
     private String description;
+    private double duration;
+    private int isNow;
 
-    public TimeLineModel(String artist, Date date, String genre, String description) {
+    public TimeLineModel(String artist, Date date, String genre, String description, double duration) {
         this.artist = artist;
-        this.date = date.toString();
+        this.date = date;
+        this.stringDate = date.toString();
         this.genre = genre;
         this.description = description;
+        this.duration = duration;
+        if (!setIsNow()) isNow = 1;
     }
 
     String getArtist() {
         return artist;
     }
 
-    public String getDate() {
+    public Date getDate() {
         return date;
     }
 
@@ -35,6 +41,18 @@ public class TimeLineModel implements Parcelable {
         return description;
     }
 
+    double getDuration() {
+        return duration;
+    }
+
+    String getStringDate() {
+        return stringDate;
+    }
+
+    int getIsNow() {
+        return isNow;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -43,16 +61,22 @@ public class TimeLineModel implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(this.artist);
-        dest.writeString(date);
+        dest.writeString(date.toString());
         dest.writeString(genre);
         dest.writeString(description);
+        dest.writeDouble(duration);
+        if(!setIsNow()) isNow = 1;
+        dest.writeInt(isNow);
     }
 
     private TimeLineModel(Parcel in) {
         this.artist = in.readString();
-        this.date = in.readString();
+        this.stringDate = in.readString();
         this.genre = in.readString();
         this.description = in.readString();
+        this.duration = in.readDouble();
+        this.isNow = in.readInt();
+        date = null;
     }
 
     public static final Creator<TimeLineModel> CREATOR = new Creator<TimeLineModel>() {
@@ -66,4 +90,33 @@ public class TimeLineModel implements Parcelable {
             return new TimeLineModel[size];
         }
     };
+
+    /**
+     * Sets isNow, the int is 0 if the concert is happening at the time of creation of the timeline,
+     * 1 if the concert will happen and -1 if it already happened.
+     *
+     * @return true if the operation was successful
+     */
+    private boolean setIsNow() {
+        Date actualDate = new Date();
+        if (getDate() == null) {
+            return false;
+        }
+
+        Date endOfConcert = (Date) getDate().clone();
+
+        int duration_hours = (int)Math.abs(getDuration());
+
+        endOfConcert.setHours(endOfConcert.getHours() + duration_hours);
+        endOfConcert.setMinutes(endOfConcert.getMinutes() + (int)Math.abs(
+                (getDuration() - duration_hours)*10));
+
+        if (actualDate.after(getDate())) {
+            if (actualDate.before(endOfConcert)) isNow = 0; //concert is taking place
+            else isNow = 1; //concert is later
+        }
+        else isNow = -1; //concert happened
+
+        return true;
+    }
 }
