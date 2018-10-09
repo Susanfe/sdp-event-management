a9ab578b667567bdef9111a693985eeb019c14a5
Timothée Floure <timothee.floure@epfl.ch>
2018-10-09 09:12:44 +0200

    Move LoginActivityTest to LoginActivity's package

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/userManager/LoginActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/userManager/LoginActivityTest.java
@@ -1,4 +1,4 @@
-package ch.epfl.sweng.eventmanager;
+package ch.epfl.sweng.eventmanager.ui.userManager;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -11,6 +11,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.containsString;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -18,7 +19,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import ch.epfl.sweng.eventmanager.ui.userManager.LoginActivity;
+import ch.epfl.sweng.eventmanager.R;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
@@ -31,7 +32,7 @@ public class LoginActivityTest {
         String email = "lamb.da@domain.tld";
         String password = "secret";
 
-        onView(withId(R.id.email_field))
+        onView(ViewMatchers.withId(R.id.email_field))
                 .perform(typeText(email))
                 .perform(closeSoftKeyboard());
         onView(withId(R.id.password_field))
