ce647407eeae8c4c189d57098b1945912fdd2467
zyuiop <zyuiop@zyuiop.net>
2018-10-09 19:15:57 +0200

    Reformat a very long line

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/EventRepository.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/EventRepository.java
@@ -48,7 +48,10 @@ public class EventRepository {
 
     public LiveData<Event> getEvent(int eventId) {
         final MutableLiveData<Event> ret = new MutableLiveData<>();
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("events").child(String.valueOf(eventId));
+        DatabaseReference dbRef = FirebaseDatabase
+                .getInstance()
+                .getReference("events")
+                .child(String.valueOf(eventId));
 
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
