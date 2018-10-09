cd5386fc58742867c0b4a32fd6e4f0b3694d1674
AntoineCrettenand <lugiar13@gmail.com>
2018-10-09 11:14:40 +0200

    Add comments and not null condition in constructor

--- app/src/main/java/ch/epfl/sweng/eventmanager/room/data/JoinedEvent.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/room/data/JoinedEvent.java
@@ -5,15 +5,30 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import ch.epfl.sweng.eventmanager.repository.data.Event;
 
+import java.util.Objects;
+
+/**
+ * Defines the JoinedEvent Entity. We only store the event-id and the name of the event to minimise storage space
+ */
 @Entity(tableName = "joined_events")
 public class JoinedEvent {
 
+    /**
+     * Default constructor needed by Room to instantiate the entity
+     * @param uid event-id
+     * @param name of the event
+     */
     public JoinedEvent(int uid, String name) {
         this.uid = uid;
         this.name = name;
     }
 
+    /**
+     * Prioritize use of this constructor instead
+     * @param event not null
+     */
     public JoinedEvent(Event event) {
+        Objects.requireNonNull(event);
         this.uid = event.getId();
         this.name = event.getName();
     }
@@ -25,6 +40,8 @@ public class JoinedEvent {
     @ColumnInfo(name = "name")
     private String name;
 
+
+    /* Getters and Setters */
 
     public int getUid() {
         return uid;
