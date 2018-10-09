2efa1df09fae6969971806e33dd5212fcaf81480
Nicolas d'Argenlieu <nicolas.dargenlieu@epfl.ch>
2018-10-09 15:34:14 +0200

    Further customized TimeLine object
    Customized marker for each TimeLineView object depending on the status
    of the Concert (playing now, already happened or will happen)
    Integrated Duration display in the object
    Added 3 marker states in the drawables

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/scheduleUtils/TimeLineAdapter.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/scheduleUtils/TimeLineAdapter.java
@@ -1,8 +1,13 @@
 package ch.epfl.sweng.eventmanager.ui.eventShowcase.scheduleUtils;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.graphics.drawable.VectorDrawableCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,8 +37,9 @@ public class TimeLineAdapter extends RecyclerView.Adapter<TimeLineViewHolder> {
         return TimelineView.getTimeLineViewType(position,getItemCount());
     }
 
+    @NonNull
     @Override
-    public TimeLineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public TimeLineViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         mContext = parent.getContext();
         mLayoutInflater = LayoutInflater.from(mContext);
         View view = mLayoutInflater.inflate(R.layout.event_list_item, parent, false);
@@ -41,26 +47,51 @@ public class TimeLineAdapter extends RecyclerView.Adapter<TimeLineViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(TimeLineViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull TimeLineViewHolder holder, int position) {
 
         TimeLineModel timeLineModel = dataList.get(position);
 
         String artistText = timeLineModel.getArtist().isEmpty() ? Concert.NO_ARTIST : timeLineModel.getArtist();
-        String dateText = timeLineModel.getDate().isEmpty() ? NO_DATE : timeLineModel.getDate();
+        String dateText = timeLineModel.getStringDate().isEmpty() ? NO_DATE : timeLineModel.getStringDate();
         String genreText = timeLineModel.getGenre().isEmpty() ? Concert.NO_GENRE : timeLineModel.getGenre();
         String descriptionText = timeLineModel.getDescription().isEmpty() ? Concert.NO_DESCRIPTION : timeLineModel.getDescription();
+        String durationText = transformDuration(timeLineModel.getDuration());
 
         holder.mArtist.setText(artistText);
         holder.mDate.setText(dateText);
         holder.mGenre.setText(genreText);
         holder.mDescription.setText(descriptionText);
+        holder.mDuration.setText(durationText);
 
-        // Need to implement the TimeLine Marker's state
-
+        // Sets Marker according to the concert status
+        if (timeLineModel.getIsNow() == -1) {
+            holder.mTimelineView.setMarker(getMarkerDrawable(mContext, R.drawable.ic_marker_inactive, android.R.color.darker_gray));
+        } else if (timeLineModel.getIsNow() == 0) {
+            holder.mTimelineView.setMarker(getMarkerDrawable(mContext, R.drawable.ic_marker_active, R.color.colorPrimary));
+        } else {
+            holder.mTimelineView.setMarker(ContextCompat.getDrawable(mContext, R.drawable.ic_marker), ContextCompat.getColor(mContext, R.color.colorPrimary));
+        }
     }
 
     @Override
     public int getItemCount() {
         return (dataList !=null? dataList.size():0);
+    }
+
+    private String transformDuration(double duration) {
+        double intPart = Math.abs(duration);
+        double fractionalPart = Math.round((duration - intPart)*60 / 10d)*10d;
+        return intPart+":"+fractionalPart;
+    }
+
+    private static Drawable getMarkerDrawable(Context context, int drawableResId, int colorFilter) {
+        Drawable drawable = VectorDrawableCompat.create(context.getResources(), drawableResId, context.getTheme());
+        try {
+            drawable.setColorFilter(ContextCompat.getColor(context, colorFilter), PorterDuff.Mode.SRC_IN);
+        } catch (NullPointerException n) {
+            Log.i("ERROR", "setColorFilter method returned null in TimeLineAdapter while " +
+                    "fetching marker drawable");
+        }
+        return drawable;
     }
 }
