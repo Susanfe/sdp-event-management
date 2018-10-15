31e69214470a094916151b5c844f9fcb71f9afbd
stanislas jouven <stanislas.jouven@flaminem.com>
2018-10-15 17:42:30 +0200

    Modified test file for EventActivity

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/EventActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/EventActivityTest.java
@@ -1,5 +1,7 @@
 package ch.epfl.sweng.eventmanager;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -8,13 +10,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.eventmanager.ui.eventShowcase.EventActivity;
+import ch.epfl.sweng.eventmanager.ui.eventShowcase.MapsActivity;
+import ch.epfl.sweng.eventmanager.ui.eventShowcase.ScheduleActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.v4.media.session.MediaSessionCompatApi21.isActive;
+import static java.sql.DriverManager.println;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class EventActivityTest {
@@ -22,10 +28,18 @@ public class EventActivityTest {
     @Rule
     public final ActivityTestRule<EventActivity> mActivityRule =
             new ActivityTestRule<>(EventActivity.class);
-
+/*
     @Test
     public void goToMapWork() {
-        onView(withId(R.id.map_button)).perform(click()).check();
+        assertTrue(onView(withId(R.id.map_button)).perform(click()).getClass().equals(MapsActivity.class));
+    }*/
+
+    @Test
+    public void goToScheduleWork() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        println("class = " + getClass());
+        onView(withId(R.id.schedule_button)).perform(click());
+        //assertTrue(getClass().equals(scheduleActivity.class));
     }
 
 }
