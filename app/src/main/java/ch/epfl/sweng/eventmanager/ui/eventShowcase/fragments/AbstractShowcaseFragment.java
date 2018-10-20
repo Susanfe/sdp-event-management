0340b7953f4b365fab942753d6e659623b79d25e
Nicolas d'Argenlieu <nicolas.dargenlieu@epfl.ch>
2018-10-20 18:49:42 +0200

    Multiple fixes
    Removed unused imports
    + fixed manifest TicketActivity > EventTicketFragment
    + added My Schedule to navigation drawer
    + changed tools:context attribute value in activity_ticket.xml from
    TicketActivity > EventTicketFragment

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/fragments/AbstractShowcaseFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/fragments/AbstractShowcaseFragment.java
@@ -3,11 +3,9 @@ package ch.epfl.sweng.eventmanager.ui.eventShowcase.fragments;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.ui.eventShowcase.models.EventShowcaseModel;
 
 /**
