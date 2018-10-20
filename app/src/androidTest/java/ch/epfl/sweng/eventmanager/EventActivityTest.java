28617565a4c7280492593350aac3f35ac10dffc9
Nicolas d'Argenlieu <nicolas.dargenlieu@epfl.ch>
2018-10-20 16:37:53 +0200

    Corrected EventActivityTest that btw is idle

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/EventActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/EventActivityTest.java
@@ -1,7 +1,5 @@
 package ch.epfl.sweng.eventmanager;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -9,25 +7,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import ch.epfl.sweng.eventmanager.ui.eventShowcase.EventActivity;
-import ch.epfl.sweng.eventmanager.ui.eventShowcase.MapsActivity;
-import ch.epfl.sweng.eventmanager.ui.eventShowcase.ScheduleActivity;
+import ch.epfl.sweng.eventmanager.ui.eventShowcase.EventShowcaseActivity;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static java.sql.DriverManager.println;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class EventActivityTest {
 
     @Rule
-    public final ActivityTestRule<EventActivity> mActivityRule =
-            new ActivityTestRule<>(EventActivity.class);
+    public final ActivityTestRule<EventShowcaseActivity> mActivityRule =
+            new ActivityTestRule<>(EventShowcaseActivity.class);
 
     @Test
     public void pass() {
