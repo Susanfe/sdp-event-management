720b5ca66bbc6bcf5aeccf0857f092ef2fcfb893
stanislas jouven <stanislas.jouven@flaminem.com>
2018-10-24 15:44:15 +0200

    add test for zoom in and zoom out on EventMapFragmentTest

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/EventMapFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/EventMapFragmentTest.java
@@ -1,28 +1,42 @@
 package ch.epfl.sweng.eventmanager.ui;
 
 import android.content.Intent;
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
-import android.view.Gravity;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.ui.eventSelector.EventPickingActivity;
 import ch.epfl.sweng.eventmanager.ui.eventShowcase.EventShowcaseActivity;
-import ch.epfl.sweng.eventmanager.ui.eventShowcase.fragments.EventMapFragment;
 
+import static android.os.SystemClock.sleep;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.os.SystemClock.sleep;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
 
+@RunWith(AndroidJUnit4.class)
 public class EventMapFragmentTest {
 
     @Rule
@@ -30,7 +44,7 @@ public class EventMapFragmentTest {
             new ActivityTestRule<>(EventShowcaseActivity.class);
 
     @Test
-    public void testMap() {
+    public void eventMapTest() {
         Intent intent = new Intent();
         // Opens Sysmic Event
         intent.putExtra(EventPickingActivity.SELECTED_EVENT_ID, 2);
@@ -42,7 +56,49 @@ public class EventMapFragmentTest {
         // Display Schedule Events
         onView(withId(R.id.nav_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_map));
+        sleep(5000);
+
+
+        ViewInteraction imageView = onView(
+                allOf(withContentDescription("Zoom in"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.RelativeLayout")),
+                                        2),
+                                0),
+                        isDisplayed()));
+        imageView.perform(click());
+
+
+
+        ViewInteraction imageView3 = onView(
+                allOf(withContentDescription("Zoom out"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.RelativeLayout")),
+                                        2),
+                                1),
+                        isDisplayed()));
+        imageView3.perform(click());
 
     }
 
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
