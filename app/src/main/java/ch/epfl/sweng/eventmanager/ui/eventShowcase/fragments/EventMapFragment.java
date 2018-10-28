429fbf2a2bdbf679fa90dfc250f97e58afe1e7a6
stanislas jouven <stanislas.jouven@flaminem.com>
2018-10-28 17:22:30 +0100

    add the current location feature and add a button to go to the current location of the phone

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/fragments/EventMapFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/fragments/EventMapFragment.java
@@ -1,14 +1,21 @@
 package ch.epfl.sweng.eventmanager.ui.eventShowcase.fragments;
 
+import android.Manifest;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.repository.data.EventLocation;
 import ch.epfl.sweng.eventmanager.repository.data.Spot;
-import ch.epfl.sweng.eventmanager.repository.data.SpotType;
-import ch.epfl.sweng.eventmanager.ui.eventShowcase.models.ScheduleViewModel;
 import ch.epfl.sweng.eventmanager.ui.eventShowcase.models.SpotsModel;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,8 +24,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Display the map and his features
@@ -34,6 +39,7 @@ public class EventMapFragment extends AbstractShowcaseFragment {
     private ClusterManager<Spot> mClusterManager;
     protected SpotsModel spotsModel;
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     public EventMapFragment() {
         // Required empty public constructor
@@ -64,11 +70,10 @@ public class EventMapFragment extends AbstractShowcaseFragment {
                     setUpClusterer();
                 }
             });
-        }
 
+        }
         TextView textView = (TextView) view.findViewById(R.id.text_test);
         textView.setText("everything is ready");
-
     }
 
     private void setUpMap() {
@@ -80,13 +85,15 @@ public class EventMapFragment extends AbstractShowcaseFragment {
             LatLng place = loc.getPosition().asLatLng();
             mMap.addMarker(new MarkerOptions().position(place).title(loc.getName()));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, ZOOMLEVEL));
-
             mMap.getUiSettings().setCompassEnabled(true);
             mMap.getUiSettings().setZoomControlsEnabled(true);
             mMap.getUiSettings().setMapToolbarEnabled(true);
+            enableMyLocationIfPermitted();
+            mMap.setOnMyLocationButtonClickListener(onMyLocationButtonClickListener);
+            mMap.setOnMyLocationClickListener(onMyLocationClickListener);
+
+
         });
-
-
     }
 
     private void setUpClusterer() {
@@ -114,5 +121,36 @@ public class EventMapFragment extends AbstractShowcaseFragment {
         });
     }
 
+    private void enableMyLocationIfPermitted() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private GoogleMap.OnMyLocationButtonClickListener onMyLocationButtonClickListener = new GoogleMap.OnMyLocationButtonClickListener() {
+        @Override
+        public boolean onMyLocationButtonClick() {
+            Log.i("test:", "buttonCkick");
+            Toast.makeText(getActivity(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+            // Return false so that we don't consume the event and the default behavior still occurs
+            // (the camera animates to the user's current position).
+            return false;
+        }
+    };
+
+    private GoogleMap.OnMyLocationClickListener onMyLocationClickListener =  new GoogleMap.OnMyLocationClickListener() {
+        @Override
+        public void onMyLocationClick(@NonNull Location location) {
+            Log.i("test:", "buttonCkickListener");
+            Toast.makeText(getActivity(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
+        }
+    };
 
 }
