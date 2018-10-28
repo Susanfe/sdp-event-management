020f79c54b31dcca9080e553f31b1ed966d58ce3
zbirobin <robin.zbinden@epfl.ch>
2018-10-28 17:08:32 +0100

    Use UIautomator to test the markers

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/EventMapFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/EventMapFragmentTest.java
@@ -5,7 +5,12 @@ import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,13 +20,17 @@ import ch.epfl.sweng.eventmanager.ui.eventSelector.EventPickingActivity;
 import ch.epfl.sweng.eventmanager.ui.eventShowcase.EventShowcaseActivity;
 
 import static android.os.SystemClock.sleep;
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
 public class EventMapFragmentTest {
+
+    private final static int DIFFERENCE_TO_SNIPPET = 80;
 
     @Rule
     public final ActivityTestRule<EventShowcaseActivity> mActivityRule =
@@ -37,9 +46,21 @@ public class EventMapFragmentTest {
         onView(withId(R.id.drawer_layout))
                 .perform(DrawerActions.open());
 
-        // Display Schedule Events
+        // Display Map Events
         onView(withId(R.id.nav_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_map));
         onView(withId(R.id.text_test)).check(matches(withText("everything is ready")));
+
+        // Clicks on the EPFL marker
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        UiObject marker = device.findObject(new UiSelector().descriptionContains("EPFL"));
+        try {
+            marker.click();
+            int x = marker.getBounds().centerX();
+            int y = marker.getBounds().centerY();
+            device.click(x, y-DIFFERENCE_TO_SNIPPET);
+        } catch (UiObjectNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
