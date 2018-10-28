47843fa9c5b20414a056166dff7a0aaa233fc55d
zyuiop <zyuiop@zyuiop.net>
2018-10-28 16:35:08 +0100

    Add twitter integration

---
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/NewsOrTweet.java
@@ -0,0 +1,70 @@
+package ch.epfl.sweng.eventmanager.repository.data;
+
+import android.support.annotation.NonNull;
+import ch.epfl.sweng.eventmanager.ui.eventShowcase.fragments.NewsFragment;
+import com.twitter.sdk.android.core.models.Tweet;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Louis Vialar
+ */
+public class NewsOrTweet implements Comparable<NewsOrTweet> {
+    public static final int TYPE_TWEET = 1;
+    public static final int TYPE_NEWS = 2;
+
+    private final News news;
+    private final Tweet tweet;
+    private final long time;
+
+    NewsOrTweet(News news) {
+        this.news = news;
+        this.tweet = null;
+        this.time = news.getDate();
+    }
+
+    NewsOrTweet(Tweet tweet) {
+        this.news = null;
+        this.tweet = tweet;
+        this.time = Date.parse(tweet.createdAt);
+    }
+
+    public News getNews() {
+        return news;
+    }
+
+    public Tweet getTweet() {
+        return tweet;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    @Override
+    public int compareTo(@NonNull NewsOrTweet o) {
+        return Long.compare(o.time, time);
+    }
+
+    public int getItemType() {
+        return this.tweet == null ? TYPE_NEWS : TYPE_TWEET;
+    }
+
+    public static List<NewsOrTweet> mergeLists(List<News> news, List<Tweet> tweets) {
+        List<NewsOrTweet> target = new ArrayList<>();
+        for (News n : news) target.add(new NewsOrTweet(n));
+        for (Tweet t : tweets) target.add(new NewsOrTweet(t));
+        return target;
+    }
+
+    @Override
+    public String toString() {
+        return "NewsOrTweet{" +
+                "news=" + news +
+                ", tweet=" + tweet +
+                ", time=" + time +
+                '}';
+    }
+}
--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/fragments/NewsFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/fragments/NewsFragment.java
@@ -1,10 +1,13 @@
 package ch.epfl.sweng.eventmanager.ui.eventShowcase.fragments;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Transformations;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +16,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.repository.data.News;
+import ch.epfl.sweng.eventmanager.repository.data.NewsOrTweet;
 import ch.epfl.sweng.eventmanager.ui.eventShowcase.models.NewsViewModel;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.models.TweetBuilder;
+import com.twitter.sdk.android.tweetui.CompactTweetView;
+import com.twitter.sdk.android.tweetui.TimelineResult;
+import com.twitter.sdk.android.tweetui.UserTimeline;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,6 +38,7 @@ public class NewsFragment extends AbstractShowcaseFragment {
     @BindView(R.id.news_empty_tv)
     TextView emptyListTextView;
     private NewsAdapter newsAdapter;
+
     public NewsFragment() {
         super(R.layout.fragment_news);
     }
@@ -54,15 +67,17 @@ public class NewsFragment extends AbstractShowcaseFragment {
             model = ViewModelProviders.of(requireActivity()).get(NewsViewModel.class);
         }
 
-        this.model.getNews().observe(this, news -> {
+        LiveData<String> twitterName = Transformations.map(super.model.getEvent(), event -> event.getTwitterName());
+
+        this.model.getNews(twitterName).observe(this, news -> {
             if (news != null && news.size() > 0) {
                 emptyListTextView.setVisibility(View.GONE);
                 recyclerView.setVisibility(View.VISIBLE);
-                newsAdapter.setNews(news);
+                newsAdapter.setContent(news);
             } else {
                 emptyListTextView.setVisibility(View.VISIBLE);
                 recyclerView.setVisibility(View.GONE);
-                newsAdapter.setNews(Collections.emptyList());
+                newsAdapter.setContent(Collections.emptyList());
             }
         });
     }
@@ -74,8 +89,9 @@ public class NewsFragment extends AbstractShowcaseFragment {
         model = ViewModelProviders.of(requireActivity()).get(NewsViewModel.class);
     }
 
-    public static class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
-        private List<News> news;
+    public static class NewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        private List<NewsOrTweet> news;
 
         public NewsAdapter() {
             this.news = Collections.emptyList();
@@ -83,17 +99,33 @@ public class NewsFragment extends AbstractShowcaseFragment {
 
         // Create new views (invoked by the layout manager)
         @Override
-        public NewsAdapter.NewsViewHolder onCreateViewHolder(ViewGroup parent,
-                                                             int viewType) {
-            View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_news, parent, false);
-            return new NewsViewHolder(v);
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                          int viewType) {
+
+            if (viewType == NewsOrTweet.TYPE_TWEET) {
+                final Tweet tweet = new TweetBuilder().build();
+                final CompactTweetView compactTweetView = new CompactTweetView(parent.getContext(), tweet);
+
+                return new TweetViewHolder(compactTweetView);
+            } else {
+                View v = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_news, parent, false);
+                return new NewsViewHolder(v);
+            }
         }
 
         // Replace the contents of a view (invoked by the layout manager)
         @Override
-        public void onBindViewHolder(NewsAdapter.NewsViewHolder holder, int position) {
-            holder.bind(news.get(position));
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            if (holder instanceof NewsViewHolder)
+                ((NewsViewHolder) holder).bind(news.get(position).getNews());
+            else if (holder instanceof TweetViewHolder)
+                ((CompactTweetView) ((TweetViewHolder) holder).itemView).setTweet(news.get(position).getTweet());
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return news.get(position).getItemType();
         }
 
         @Override
@@ -101,11 +133,21 @@ public class NewsFragment extends AbstractShowcaseFragment {
             return news.size();
         }
 
-        public void setNews(List<News> news) {
+        public void setContent(List<NewsOrTweet> news) {
             this.news = news;
+
+            Collections.sort(this.news);
+
+            this.notifyDataSetChanged();
         }
 
-        final class NewsViewHolder extends RecyclerView.ViewHolder {
+        protected static final class TweetViewHolder extends RecyclerView.ViewHolder {
+            public TweetViewHolder(CompactTweetView itemView) {
+                super(itemView);
+            }
+        }
+
+        protected static final class NewsViewHolder extends RecyclerView.ViewHolder {
             @BindView(R.id.text_news_content)
             TextView content;
             @BindView(R.id.text_news_date)
