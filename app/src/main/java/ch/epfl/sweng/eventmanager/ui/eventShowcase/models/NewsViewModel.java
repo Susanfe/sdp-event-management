92eb9fe8808d0ac3633c08ba5630f135e385a017
zyuiop <zyuiop@zyuiop.net>
2018-10-28 15:19:46 +0100

    Remove unused imports

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/models/NewsViewModel.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/models/NewsViewModel.java
@@ -1,20 +1,12 @@
 package ch.epfl.sweng.eventmanager.ui.eventShowcase.models;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.Transformations;
 import android.arch.lifecycle.ViewModel;
-import android.content.Context;
-import ch.epfl.sweng.eventmanager.repository.JoinedScheduleItemRepository;
 import ch.epfl.sweng.eventmanager.repository.NewsRepository;
-import ch.epfl.sweng.eventmanager.repository.ScheduledItemRepository;
-import ch.epfl.sweng.eventmanager.repository.data.JoinedScheduleItem;
 import ch.epfl.sweng.eventmanager.repository.data.News;
-import ch.epfl.sweng.eventmanager.repository.data.ScheduledItem;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class NewsViewModel extends ViewModel {
     private LiveData<List<News>> news;
