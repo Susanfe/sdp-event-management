4bc8b6b4696113b88fbffba643aa2672f5b443c8
Nicolas d'Argenlieu <nicolas.dargenlieu@epfl.ch>
2018-10-28 21:05:10 +0100

    Changed EventOrganizer instantiation

--- app/src/test/java/ch/epfl/sweng/eventmanager/repository/data/EventTest.java
+++ app/src/test/java/ch/epfl/sweng/eventmanager/repository/data/EventTest.java
@@ -12,31 +12,31 @@ import static org.junit.Assert.*;
  */
 public class EventTest {
     List<Spot> spotList = new ArrayList<>();
-    private final EventOrganizer orga1 = new EventOrganizer(1, "Orga1", "Organizer 1", null);
-    private final EventOrganizer orga2 = new EventOrganizer(2, "Orga2", "Organizer 2", null);
+    private final EventOrganizer orga1 = new EventOrganizer(1, "Orga1", "Organizer 1", null, null);
+    private final EventOrganizer orga2 = new EventOrganizer(2, "Orga2", "Organizer 2", null, null);
     private final Event ev1 = new Event(1, "Event1", "Event Description 1", orga1, null, null,spotList);
     private final Event ev2 = new Event(2, "Event2", "Event Description 2", orga2, null, null,spotList);
 
     @Test
-    public void getIdTest() throws Exception {
+    public void getIdTest() {
         assertEquals(1, ev1.getId());
         assertEquals(2, ev2.getId());
     }
 
     @Test
-    public void getNameTest() throws Exception {
+    public void getNameTest() {
         assertEquals("Event1", ev1.getName());
         assertEquals("Event2", ev2.getName());
     }
 
     @Test
-    public void getDescriptionTest() throws Exception {
+    public void getDescriptionTest() {
         assertEquals("Event Description 1", ev1.getDescription());
         assertEquals("Event Description 2", ev2.getDescription());
     }
 
     @Test
-    public void getOrganizerTest() throws Exception {
+    public void getOrganizerTest() {
         assertEquals(orga1, ev1.getOrganizer());
         assertEquals(orga2, ev2.getOrganizer());
     }
