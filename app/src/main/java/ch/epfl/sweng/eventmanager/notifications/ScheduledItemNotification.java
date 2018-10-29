9893cbbaaf90f4051f9b2b37fc2c8800f6ce83ab
AntoineCrettenand <lugiar13@gmail.com>
2018-10-29 22:10:04 +0100

    Complete with javadoc and exception handling

--- app/src/main/java/ch/epfl/sweng/eventmanager/notifications/ScheduledItemNotification.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/notifications/ScheduledItemNotification.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.repository.data.ScheduledItem;
@@ -14,9 +15,17 @@ import java.util.Date;
 
 public class ScheduledItemNotification {
     private static final String CHANNEL_ID = "notify_001";
-    private static final String CHANNEL_NAME = "Channel human readable title";
+    private static final String CHANNEL_NAME = "Scheduled Notifications";
+    private static final Integer TIME_BEFORE_START = 600000; // For now 10mn
 
-    public static void scheduleNotification(Context context, ScheduledItem scheduledItem) {
+    /**
+     * Create a new Notification based on given {@link ScheduledItem}. The notification is broadcast
+     * to {@link NotificationPublisher} and is displayed TIME_BEFORE_START (in milliseconds) before the item starts.
+     *
+     * @param context       not null
+     * @param scheduledItem Scheduled Item
+     */
+    public static void scheduleNotification(@NonNull Context context, ScheduledItem scheduledItem) {
 
         setupNotificationChannel(context);
 
@@ -26,32 +35,48 @@ public class ScheduledItemNotification {
         Intent notificationIntent = new Intent(context, NotificationPublisher.class);
         notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, scheduledItem.getId().hashCode());
         notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
-
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, scheduledItem.getId().hashCode(), notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        long futureInMillis = SystemClock.elapsedRealtime() + 10000;
+        long future = SystemClock.elapsedRealtime() + getTimeTo(scheduledItem) - TIME_BEFORE_START;
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, future, pendingIntent);
     }
 
-    public static void unscheduleNotification(Context context, ScheduledItem scheduledItem) {
+    /**
+     * Deletes the Notification linked to {@link ScheduledItem}
+     *
+     * @param context       not null
+     * @param scheduledItem Scheduled Item
+     */
+    public static void unscheduleNotification(@NonNull Context context, ScheduledItem scheduledItem) {
         Intent intent = new Intent(context, NotificationPublisher.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), scheduledItem.getId().hashCode(), intent, 0);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), scheduledItem.getId().hashCode(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager am = (AlarmManager) context.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
         am.cancel(pendingIntent);
         // Cancel the `PendingIntent` after you've canceled the alarm
         pendingIntent.cancel();
     }
 
-    private static long getTimeMillisTo(ScheduledItem scheduledItem) {
+    /**
+     * Compute the time remaining until {@param scheduledItem} starts
+     *
+     * @param scheduledItem, not null
+     * @return time in milliseconds
+     * @throws NullPointerException  if the date of {@param scheduledItem} is null
+     * @throws IllegalStateException if {@param scheduledItem} is already past
+     */
+    private static long getTimeTo(@NonNull ScheduledItem scheduledItem) {
         Date scheduledItemDate = scheduledItem.getDate();
-        // TODO better handle the cases where Date == null and event is past
         if (scheduledItemDate == null)
-            return 0;
-        return scheduledItem.getDate().getTime() - System.currentTimeMillis();
+            throw new NullPointerException();
+        long timeUntil = scheduledItem.getDate().getTime() - System.currentTimeMillis();
+
+        if (timeUntil < 0) // Event is past
+            throw new IllegalStateException();
+        else return timeUntil;
     }
 
-    private static Notification getNotification(Context context, ScheduledItem scheduledItem) {
+    private static Notification getNotification(@NonNull Context context, ScheduledItem scheduledItem) {
         // Set up on tap action TODO display my_schedule fragment when tap on notification
         Intent intent = new Intent(context, EventPickingActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -61,7 +86,7 @@ public class ScheduledItemNotification {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setContentTitle(scheduledItem.getArtist() + " concert will start soon")
                 .setContentText(scheduledItem.getDescription())
-                .setSmallIcon(R.drawable.ic_marker_active)
+                .setSmallIcon(R.drawable.ic_marker_active) // TODO define a small icon for notifications
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent);
@@ -69,7 +94,7 @@ public class ScheduledItemNotification {
         return builder.build();
     }
 
-    private static void setupNotificationChannel(Context context) {
+    private static void setupNotificationChannel(@NonNull Context context) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         // Setup Notification Channel
