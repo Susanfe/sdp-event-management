2f6936bc735aeceeb66ac0f31005d1ecbc45d788
AntoineCrettenand <lugiar13@gmail.com>
2018-11-02 18:04:54 +0100

    Change Tests to change of event attributes

--- app/src/test/java/ch/epfl/sweng/eventmanager/repository/data/EventTest.java
+++ app/src/test/java/ch/epfl/sweng/eventmanager/repository/data/EventTest.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.eventmanager.repository.data;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -15,17 +16,24 @@ import static org.junit.Assert.*;
 public class EventTest {
     private List<Spot> spotList = new ArrayList<>();
     private EventLocation l1 = new EventLocation("Fake1", new Position(10, 10));
-    private final Date when = new Calendar.Builder()
+    private final Date start = new Calendar.Builder()
             .setFields(Calendar.YEAR, 2018,
                     Calendar.MONTH, Calendar.DECEMBER,
                     Calendar.DAY_OF_MONTH, 25,
                     Calendar.HOUR_OF_DAY, 12,
                     Calendar.MINUTE, 0,
                     Calendar.SECOND, 0).build().getTime();
+    private final Date end = new Calendar.Builder()
+            .setFields(Calendar.YEAR, 2018,
+                    Calendar.MONTH, Calendar.DECEMBER,
+                    Calendar.DAY_OF_MONTH, 26,
+                    Calendar.HOUR_OF_DAY, 12,
+                    Calendar.MINUTE, 0,
+                    Calendar.SECOND, 0).build().getTime();
     private final EventOrganizer orga1 = new EventOrganizer(1, "Orga1", "Organizer 1", null, null);
     private final EventOrganizer orga2 = new EventOrganizer(2, "Orga2", "Organizer 2", null, null);
-    private final Event ev1 = new Event(1, "Event1", "Event Description 1", when,  orga1, null, null,spotList, null);
-    private final Event ev2 = new Event(2, "Event2", "Event Description 2", when, orga2, null, l1,spotList, null);
+    private final Event ev1 = new Event(1, "Event1", "Event Description 1", start, end,  orga1, null, null,spotList, null);
+    private final Event ev2 = new Event(2, "Event2", "Event Description 2", start, end, orga2, null, l1,spotList, null);
 
     @Test
     public void getIdTest() {
@@ -47,8 +55,13 @@ public class EventTest {
 
     @Test
     public void getDateTest(){
-        assertEquals(ev1.getDate(), when);
-        assertEquals(ev2.getDate(), when);
+        assertEquals(ev1.getBeginDate(), start);
+        assertEquals(ev1.getEndDate(), end);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getWrongDateTest(){
+        final Event falseDate = new Event(1, "Event1", "Event Description 1", end, start,  orga1, null, null,spotList, null);
     }
 
     @Test
@@ -65,7 +78,7 @@ public class EventTest {
         assertEquals(1, ev1.getSpotList().size());
 
         spotList.add(new Spot("PMU", SpotType.BAR, 10, 10));
-        Event fake1 = new Event(-1, "Fake1", null, new Date(0), null, null, null, spotList, null);
+        Event fake1 = new Event(-1, "Fake1", null, new Date(0), new Date(0), null, null, null, spotList, null);
         assertEquals(1, fake1.getSpotList().size());
     }
 
@@ -74,5 +87,14 @@ public class EventTest {
         assertNull(ev1.getLocation());
         EventLocation fake1 = new EventLocation(l1.getName(), l1.getPosition());
         assertEquals(fake1, ev2.getLocation());
+    }
+
+    @Test
+    public void dateAsString() {
+        SimpleDateFormat f = new SimpleDateFormat("dd MMMM yyyy 'at' kk'h'mm");
+        String start_s = f.format(start);
+        String end_s = f.format(end);
+        assertEquals(start_s, ev1.beginDateAsString());
+        assertEquals(end_s, ev1.endDateAsString());
     }
 }
