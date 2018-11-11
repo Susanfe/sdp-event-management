f2bfd5279bd5c871d79f1eb405560c78ac747e02
Timothée Floure <timothee.floure@fnux.ch>
2018-11-11 12:04:16 +0100

    Event UI: Move the changeFragment method into its own class for easier sharing

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventShowcaseActivity.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventShowcaseActivity.java
@@ -31,7 +31,7 @@ import ch.epfl.sweng.eventmanager.users.Session;
 import ch.epfl.sweng.eventmanager.viewmodel.ViewModelFactory;
 import dagger.android.AndroidInjection;
 
-public class EventShowcaseActivity extends AppCompatActivity
+public class EventShowcaseActivity extends MultiFragmentActivity
         implements NavigationView.OnNavigationItemSelectedListener{
     private static final String TAG = "EventShowcaseActivity";
 
@@ -86,6 +86,10 @@ public class EventShowcaseActivity extends AppCompatActivity
                             return;
                         }
 
+                        // Set window title
+                        setTitle("Admin: " + ev.getName());
+
+                        // Only display the admin button is the user is cleared for ADMIN rights
                         if (Session.isLoggedIn() && Session.isClearedFor(Role.ADMIN, ev)) {
                             MenuItem adminMenuItem = navigationView.getMenu().findItem(R.id.nav_admin);
                             adminMenuItem.setVisible(true);
@@ -147,45 +151,5 @@ public class EventShowcaseActivity extends AppCompatActivity
         }
 
         return true;
-    }
-
-    /**
-     * Change the current displayed fragment by a new one.
-     * - if the fragment is in backstack, it will pop it
-     * - if the fragment is already displayed (trying to change the fragment with the same), it will not do anything
-     *
-     * @param frag            the new fragment to display
-     * @param saveInBackstack if we want the fragment to be in backstack
-     */
-    private void changeFragment(Fragment frag, boolean saveInBackstack) {
-        String backStateName = ((Object) frag).getClass().getName();
-
-        try {
-            FragmentManager manager = getSupportFragmentManager();
-            boolean fragmentPopped = manager.popBackStackImmediate(backStateName, 0);
-
-            if (!fragmentPopped && manager.findFragmentByTag(backStateName) == null) {
-                // fragment not in back stack, create it.
-                FragmentTransaction transaction = manager.beginTransaction();
-
-                transaction.replace(R.id.content_frame, frag, backStateName);
-
-                if (saveInBackstack) {
-                    Log.d(TAG, "Change Fragment: addToBackTack " + backStateName);
-                    transaction.addToBackStack(backStateName);
-                } else {
-                    Log.d(TAG, "Change Fragment: NO addToBackTack");
-                }
-
-                transaction.commit();
-            } else {
-                // custom effect if fragment is already instanciated
-            }
-        } catch (IllegalStateException exception) {
-            Log.w(TAG,
-                    "Unable to commit fragment, could be activity as been killed in background. "
-                            + exception.toString()
-            );
-        }
     }
 }
