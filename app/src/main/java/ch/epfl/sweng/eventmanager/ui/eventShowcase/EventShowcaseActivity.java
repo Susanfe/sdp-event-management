f9b0680cf010b87afb6dda852527a862b5e0a6eb
NicoDarge <nico.darge@gmail.com>
2018-11-11 19:40:47 +0100

    Refactored EventShowcaseActivity to use Toolbar.setTitle
    Instead of Activity.setTitle => modification of default ActionBar
    ActionBar is replaced by v7 Toolbar for customization

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/EventShowcaseActivity.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/eventShowcase/EventShowcaseActivity.java
@@ -262,6 +262,11 @@ public class EventShowcaseActivity extends AppCompatActivity
 
 
     }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        toolbar.setTitle(title);
+    }
 }
 
 
