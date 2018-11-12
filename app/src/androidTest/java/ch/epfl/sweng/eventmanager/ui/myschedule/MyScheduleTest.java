79049f1edf7a27119656544ea8ea59da9dfa10ca
zyuiop <zyuiop@zyuiop.net>
2018-11-12 13:45:04 +0100

    Cancel google calendar opeining

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/myschedule/MyScheduleTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/myschedule/MyScheduleTest.java
@@ -1,9 +1,12 @@
 package ch.epfl.sweng.eventmanager.ui.myschedule;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.Gravity;
@@ -15,6 +18,7 @@ import ch.epfl.sweng.eventmanager.ui.eventShowcase.EventShowcaseActivity;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,7 +27,9 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
@@ -48,6 +54,13 @@ public class MyScheduleTest {
                 return matcher.matches(view) && currentIndex++ == index;
             }
         };
+    }
+
+    @Before
+    public void setup() {
+        Intents.init();
+
+        Intents.intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
     }
 
     @Test
@@ -84,8 +97,6 @@ public class MyScheduleTest {
         SystemClock.sleep(400);
         onView(allOf(isDisplayed(),withId(R.id.addToCalendar))).perform(click());
         SystemClock.sleep(400);
-        pressBack();
-        pressBack();
     }
 
 }
