5328d529c2979388b2eb4e3238212e8b34f25f3a
zyuiop <zyuiop@zyuiop.net>
2018-11-13 04:11:34 +0100

    Test for login activity errors

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/ticketing/activities/LoginTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/ticketing/activities/LoginTest.java
@@ -4,7 +4,10 @@ import android.os.SystemClock;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.intent.matcher.IntentMatchers;
 import ch.epfl.sweng.eventmanager.R;
+import ch.epfl.sweng.eventmanager.ToastMatcher;
 import ch.epfl.sweng.eventmanager.test.ticketing.MockStacks;
+import ch.epfl.sweng.eventmanager.ticketing.ErrorCodes;
+import ch.epfl.sweng.eventmanager.ticketing.data.ApiResult;
 import ch.epfl.sweng.eventmanager.ui.ticketing.TicketingActivity;
 import ch.epfl.sweng.eventmanager.ui.ticketing.TicketingConfigurationPickerActivity;
 import ch.epfl.sweng.eventmanager.ui.ticketing.TicketingLoginActivity;
@@ -13,8 +16,11 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collections;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 
 public class LoginTest extends ActivityTest<TicketingLoginActivity> {
@@ -56,6 +62,22 @@ public class LoginTest extends ActivityTest<TicketingLoginActivity> {
         onView(withId(R.id.email_sign_in_button)).perform(click());
 
         Intents.assertNoUnverifiedIntents();
+
+        Assert.assertFalse(getTicketingService().isLoggedIn());
+    }
+
+    @Test
+    public void testFailingLogin() {
+        Assert.assertFalse(getTicketingService().isLoggedIn());
+
+        getTicketingService().failNextWith(Collections.singletonList(new ApiResult.ApiError(ErrorCodes.NOT_FOUND.getCode())));
+
+        onView(withId(R.id.email)).perform(typeText(MockStacks.AUTHORIZED_USER), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText(MockStacks.PASSWORD), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+
+        onView(withText(Matchers.startsWith("Erreur de connexion"))).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+
 
         Assert.assertFalse(getTicketingService().isLoggedIn());
     }
