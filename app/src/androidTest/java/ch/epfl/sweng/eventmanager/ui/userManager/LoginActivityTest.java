56265071431df75f0bdb9ee3c7ed67ca62b0ea02
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2018-11-13 23:26:52 +0100

    fixed back nav & tests

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/userManager/LoginActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/userManager/LoginActivityTest.java
@@ -1,6 +1,20 @@
 package ch.epfl.sweng.eventmanager.ui.userManager;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.Espresso.openContextualActionModeOverflowMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.containsString;
+
 import android.content.Context;
+import android.content.Intent;
 import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.NoMatchingViewException;
@@ -8,24 +22,20 @@ import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
-import ch.epfl.sweng.eventmanager.R;
-import ch.epfl.sweng.eventmanager.users.Session;
+
 import junit.framework.AssertionFailedError;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
+import ch.epfl.sweng.eventmanager.R;
+import ch.epfl.sweng.eventmanager.users.Session;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
-    @Rule
-    public final ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(LoginActivity.class);
     int MAX_RETRY_COUNT = 10;
 
     @Before
@@ -33,31 +43,45 @@ public class LoginActivityTest {
         Session.enforceDummySessions();
     }
 
+    @Rule
+    public final ActivityTestRule<LoginActivity> mActivityRule =
+            new ActivityTestRule<>(LoginActivity.class);
+
     @Test
     public void testSuccessfulLogin() {
         String email = "lamb.da@domain.tld";
         String password = "secret";
 
-        onView(ViewMatchers.withId(R.id.email_field)).perform(typeText(email)).perform(closeSoftKeyboard());
-        onView(withId(R.id.password_field)).perform(typeText(password)).perform(closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.email_field))
+                .perform(typeText(email))
+                .perform(closeSoftKeyboard());
+        onView(withId(R.id.password_field))
+                .perform(typeText(password))
+                .perform(closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
         SystemClock.sleep(1000);
 
         for (int i = 0; i < MAX_RETRY_COUNT; i++) {
             try {
                 SystemClock.sleep(1000);
-                onView(withId(R.id.sign_in_progress_bar)).check(matches(isDisplayed()));
+                onView(withId(R.id.sign_in_progress_bar))
+                        .check(matches(isDisplayed()));
                 break;
             } catch (AssertionFailedError e) {
                 Log.w("testSuccessfulLogin", "Waiting for authentication...");
-            } catch (NoMatchingViewException e) {
+            } catch(NoMatchingViewException e) {
                 // If the view does not exist, we switched to another activity!
                 break;
             }
         }
-        onView(withId(R.id.logout_button)).perform(click());
-    }
 
+        onView(withId(R.id.main_text))
+                .check(matches(withText(containsString(email))));
+
+        onView(withId(R.id.logout_btn))
+                .perform(click());
+
+    }
 
     @Test
     @Ignore("Not handled by the dummy authentication")
@@ -68,24 +92,30 @@ public class LoginActivityTest {
         String invalidCredentialError = "The password is invalid or the user does not have a password.";
 
 
-        onView(withId(R.id.email_field)).perform(typeText(email)).perform(closeSoftKeyboard());
-        onView(withId(R.id.password_field)).perform(typeText(password)).perform(closeSoftKeyboard());
+        onView(withId(R.id.email_field))
+                .perform(typeText(email))
+                .perform(closeSoftKeyboard());
+        onView(withId(R.id.password_field))
+                .perform(typeText(password))
+                .perform(closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
 
         for (int i = 0; i < MAX_RETRY_COUNT; i++) {
             try {
                 SystemClock.sleep(1000);
-                onView(withId(R.id.sign_in_progress_bar)).check(matches(isDisplayed()));
+                onView(withId(R.id.sign_in_progress_bar))
+                        .check(matches(isDisplayed()));
                 break;
             } catch (AssertionFailedError e) {
                 Log.w("testSuccessfulLogin", "Waiting for authentication...");
-            } catch (NoMatchingViewException e) {
+            } catch(NoMatchingViewException e) {
                 // If the view does not exist, we switched to another activity!
                 break;
             }
         }
 
-        onView(withId(R.id.password_field)).check(matches(hasErrorText(invalidCredentialError)));
+        onView(withId(R.id.password_field))
+                .check(matches(hasErrorText(invalidCredentialError)));
     }
 
     @Test
@@ -96,14 +126,20 @@ public class LoginActivityTest {
         String invalidEmailError = getResourceString(R.string.invalid_email_activity_login);
 
         // Test empty password
-        onView(withId(R.id.email_field)).perform(typeText(email)).perform(closeSoftKeyboard());
+        onView(withId(R.id.email_field))
+                .perform(typeText(email))
+                .perform(closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
-        onView(withId(R.id.password_field)).check(matches(hasErrorText(emptyPasswordError)));
+        onView(withId(R.id.password_field))
+                .check(matches(hasErrorText(emptyPasswordError)));
 
         // Test invalid email address
-        onView(withId(R.id.password_field)).perform(typeText(password)).perform(closeSoftKeyboard());
+        onView(withId(R.id.password_field))
+                .perform(typeText(password))
+                .perform(closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
-        onView(withId(R.id.email_field)).check(matches(hasErrorText(invalidEmailError)));
+        onView(withId(R.id.email_field))
+                .check(matches(hasErrorText(invalidEmailError)));
     }
 
     private String getResourceString(int id) {
--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/userManager/DisplayAccountActivity.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/userManager/DisplayAccountActivity.java
@@ -1,11 +1,13 @@
 package ch.epfl.sweng.eventmanager.ui.userManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import ch.epfl.sweng.eventmanager.R;
+import ch.epfl.sweng.eventmanager.ui.eventSelector.EventPickingActivity;
 import ch.epfl.sweng.eventmanager.users.Session;
 
 public class DisplayAccountActivity extends AppCompatActivity {
@@ -20,12 +22,15 @@ public class DisplayAccountActivity extends AppCompatActivity {
         helpText.setText("Logged as: " + Session.getCurrentUser().getEmail());
 
 
-        Button logoutButton = (Button) findViewById(R.id.logout_button);
+        Button logoutButton = (Button) findViewById(R.id.logout_btn);
         logoutButton.setText(R.string.logout_button);
     }
 
     public void logoutThenRedirectToEventSelector(View view) {
         Session.logout();
+        Intent intent = new Intent(this,EventPickingActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
         finish();
     }
 
