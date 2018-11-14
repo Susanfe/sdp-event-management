94a0a6931be2aa8e7ec90086d3753b7ad5911613
Timothée Floure <timothee.floure@fnux.ch>
2018-11-14 21:36:11 +0100

    Event Administration: improve coverage of navigation

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventAdministrationActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventAdministrationActivityTest.java
@@ -1,6 +1,9 @@
 package ch.epfl.sweng.eventmanager.ui.event.interaction;
 
 import android.content.Intent;
+import android.os.SystemClock;
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -8,8 +11,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.ui.event.selection.EventPickingActivity;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.Espresso.onIdle;
 
 @RunWith(AndroidJUnit4.class)
@@ -24,7 +30,13 @@ public class EventAdministrationActivityTest {
         intent.putExtra(EventPickingActivity.SELECTED_EVENT_ID, 1);
         mActivityRule.launchActivity(intent);
 
-        // The administration activity is currently empty
+        // Open drawer
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+
+        onIdle();
+
+        // Switch to the 'visitor' section
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_showcase));
 
         onIdle();
     }
