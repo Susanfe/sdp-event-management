85ebe42f362439fc825818e90d2f1451cb53d93f
stanislas jouven <stanislasjouven@gmail.com>
2018-11-14 01:21:32 +0100

    Adapted Spot class to custom clusterer

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/Spot.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/Spot.java
@@ -1,7 +1,14 @@
 package ch.epfl.sweng.eventmanager.repository.data;
 
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import ch.epfl.sweng.eventmanager.R;
 
 /**
  * Represents a place into the event
@@ -23,11 +30,21 @@ public class Spot implements ClusterItem {
      * The position of the spot
      */
     private Position position;
+    /**
+     * The ressource picture
+     */
+    private int bitmap;
+    /**
+     * List of scheduledItem
+     */
+    private List<ScheduledItem> scheduleList;
 
-    public Spot(String title, SpotType spotType, double latitude, double longitude) {
+
+    Spot(String title, SpotType spotType, double latitude, double longitude) {
         this.title = title;
         this.spotType = spotType;
         this.position = new Position(latitude, longitude);
+        this.bitmap = R.drawable.panda;
     }
 
     public Spot() {}
@@ -39,6 +56,9 @@ public class Spot implements ClusterItem {
 
     @Override
     public String getSnippet() {
+        if(scheduleList != null && scheduleList.size() != 0){
+            return spotType == null ? null : spotType.getName() + "- Click to see the schedule";
+        }
         return spotType == null ? null : spotType.getName();
     }
 
@@ -49,5 +69,23 @@ public class Spot implements ClusterItem {
 
     public SpotType getSpotType() {
         return spotType;
+    }
+
+    public int getBitmap() {
+        Log.i("TAGIMAGE", String.valueOf(bitmap==0));
+        return bitmap;
+    }
+
+    public void setScheduleList(List<ScheduledItem> scheduleList) {
+        this.scheduleList = new LinkedList<>();
+        for (ScheduledItem s: scheduleList) {
+            if(s.getItemLocation().equals(getTitle())) {
+                this.scheduleList.add(s);
+            }
+        }
+    }
+
+    public List<ScheduledItem> getScheduleList() {
+        return scheduleList;
     }
 }
