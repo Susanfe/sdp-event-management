a625567946739cd9f3487af1105af135b8cd8d7a
zyuiop <zyuiop@zyuiop.net>
2018-11-14 23:07:20 +0100

    Fix rebase fails

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/ticketing/activities/AuthBasedScanningActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/ticketing/activities/AuthBasedScanningActivityTest.java
@@ -28,9 +28,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 public class AuthBasedScanningActivityTest extends BaseScanningActivityTest {
     public AuthBasedScanningActivityTest() {
         super(3);
-
-        dropIntents = true;
-    }
+        }
 
     @Test
     public void testLoggedOut() {
--- app/src/main/java/ch/epfl/sweng/eventmanager/EventManagerApplication.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/EventManagerApplication.java
@@ -1,11 +1,14 @@
 package ch.epfl.sweng.eventmanager;
 
 import android.app.Activity;
-import android.support.multidex.MultiDexApplication;
+import android.app.Application;
 import android.support.v4.app.Fragment;
+
 import ch.epfl.sweng.eventmanager.inject.DaggerApplicationComponent;
 import ch.epfl.sweng.eventmanager.repository.room.RoomModule;
+
 import com.twitter.sdk.android.core.Twitter;
+
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
@@ -16,7 +19,7 @@ import javax.inject.Inject;
 /**
  * @author Louis Vialar
  */
-public class EventManagerApplication extends MultiDexApplication implements HasActivityInjector, HasSupportFragmentInjector {
+public class EventManagerApplication extends Application implements HasActivityInjector, HasSupportFragmentInjector {
     @Inject
     DispatchingAndroidInjector<Activity> activityDispatchingAndroidInjector;
     @Inject
