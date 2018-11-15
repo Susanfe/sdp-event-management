208445599ce2f9f06830df8b72ed1e56cb893792
zyuiop <zyuiop@zyuiop.net>
2018-11-16 00:40:51 +0100

    Delete ticketing tests

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/test/inject/TestComponent.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/test/inject/TestComponent.java
@@ -3,11 +3,10 @@ package ch.epfl.sweng.eventmanager.test.inject;
 import ch.epfl.sweng.eventmanager.inject.ActivityBuilder;
 import ch.epfl.sweng.eventmanager.inject.ApplicationComponent;
 import ch.epfl.sweng.eventmanager.inject.ApplicationModule;
-import ch.epfl.sweng.eventmanager.test.repository.MockRepositoriesModule;
 import ch.epfl.sweng.eventmanager.repository.room.RoomModule;
+import ch.epfl.sweng.eventmanager.test.repository.MockRepositoriesModule;
 import ch.epfl.sweng.eventmanager.test.ticketing.MockTicketingModule;
 import ch.epfl.sweng.eventmanager.ui.eventShowcase.NewsFragmentTest;
-import ch.epfl.sweng.eventmanager.ui.ticketing.ScanningTest;
 import dagger.Component;
 import dagger.android.AndroidInjectionModule;
 import dagger.android.support.AndroidSupportInjectionModule;
@@ -25,7 +24,6 @@ import javax.inject.Singleton;
 @Singleton
 public interface TestComponent extends ApplicationComponent {
     void inject(NewsFragmentTest test);
-    void inject(ScanningTest test);
 
     @Component.Builder
     interface Builder extends ApplicationComponent.Builder {
--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/ticketing/starter/MultiConfigTest.java
+++
@@ -1,9 +0,0 @@
-package ch.epfl.sweng.eventmanager.ui.ticketing.starter;
-
-import ch.epfl.sweng.eventmanager.ui.ticketing.TicketingConfigurationPickerActivity;
-
-public class MultiConfigTest extends StarterTest {
-    public MultiConfigTest() {
-        super(2, TicketingConfigurationPickerActivity.class);
-    }
-}
