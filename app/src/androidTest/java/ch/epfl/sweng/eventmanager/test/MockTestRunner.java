b3381837260c9a1eb2f6c1e5dbad0f0b7150b6d2
zyuiop <zyuiop@zyuiop.net>
2018-11-15 21:43:14 +0100

    Fix MockTestRunner

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/test/MockTestRunner.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/test/MockTestRunner.java
@@ -2,9 +2,16 @@ package ch.epfl.sweng.eventmanager.test;
 
 import android.app.Application;
 import android.content.Context;
-import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.test.runner.AndroidJUnitRunner;
 
 public class MockTestRunner extends AndroidJUnitRunner {
+
+    @Override
+    public Application newApplication(ClassLoader cl, String className, Context context)
+            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+
+        // Replace the application by the test equivalent
+        return super.newApplication(cl, TestApplication.class.getName(), context);
+    }
+
 }
