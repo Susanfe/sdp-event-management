5a276105f04fc353bd2fa4e6f47fc69e2ebfc5c7
AntoineCrettenand <lugiar13@gmail.com>
2018-11-19 17:31:53 +0100

    Refactor imports

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/FeedbackRepository.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/FeedbackRepository.java
@@ -3,7 +3,6 @@ package ch.epfl.sweng.eventmanager.repository;
 import androidx.lifecycle.LiveData;
 import ch.epfl.sweng.eventmanager.repository.data.EventRating;
 import com.google.android.gms.tasks.Task;
-
 import java.util.List;
 
 public interface FeedbackRepository {
