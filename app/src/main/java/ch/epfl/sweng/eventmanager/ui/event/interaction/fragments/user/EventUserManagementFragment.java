2527f26507af9eb89deee78e839a4c95ca615197
Timothée Floure <timothee.floure@fnux.ch>
2018-11-20 11:33:40 +0100

    Event: populate "add user/role" form with proper values

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/user/EventUserManagementFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/user/EventUserManagementFragment.java
@@ -5,6 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.lang.reflect.Array;
@@ -15,6 +19,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.ui.event.interaction.fragments.AbstractShowcaseFragment;
+import ch.epfl.sweng.eventmanager.users.Role;
 
 /**
  * Allows an administrator a manage the users allowed to work with this event (= access to admin
@@ -66,6 +71,20 @@ public class EventUserManagementFragment extends AbstractShowcaseFragment {
         mUserList.setHasFixedSize(true);
         RecyclerView.LayoutManager userListLayoutManager = new LinearLayoutManager(getActivity());
         mUserList.setLayoutManager(userListLayoutManager);
+
+        // Add user/role form
+        Spinner formSpinner = view.findViewById(R.id.form_spinner);
+        String[] roles = Role.asArrayOfString();
+        ArrayAdapter<String> formSpinnerAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, roles);
+        formSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        formSpinner.setAdapter(formSpinnerAdapter);
+
+        Button formButton = view.findViewById(R.id.form_button);
+        formButton.setText(getString(R.string.add_button));
+
+        EditText formMail = view.findViewById(R.id.form_mail);
+        formMail.setHint(getString(R.string.email_field));
 
         return view;
     }
--- app/src/main/java/ch/epfl/sweng/eventmanager/users/Role.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/users/Role.java
@@ -1,9 +1,16 @@
 package ch.epfl.sweng.eventmanager.users;
 
+import java.util.Arrays;
+
 /**
  * Defines the clearance levels for authenticated users accessing an event.
  */
 public enum Role {
     ADMIN,
-    STAFF
+    STAFF;
+
+    public static String[] asArrayOfString() {
+        String rawValues = Arrays.toString(Role.values());
+        return rawValues.substring(1, rawValues.length()-1).replace(" ", "").split(",");
+    }
 }
