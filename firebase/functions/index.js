4e76a1cdccb1a651ab45547b11ccb7ffa6181642
Timothée Floure <timothee.floure@fnux.ch>
2018-11-20 11:08:49 +0100

    Add hooks to sync Firebase Auth with the realtime database

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,15 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup
+// triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.syncDatabaseOnUserCreation = functions.auth.user().onCreate((user) => {
+  return admin.database().ref("/users/" + user.uid + "/email").set(user.email);
+});
+
+exports.syncDatabaseOnUserDeletion = functions.auth.user().onDelete((user) => {
+  return admin.database().ref("/users/" + user.uid).remove();
+});
