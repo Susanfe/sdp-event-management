a5f23466f8968abccdd2b5be30c282914f9a3af5
Timothée Floure <timothee.floure@fnux.ch>
2018-11-20 15:10:20 +0100

    Add basic addUserToEvent Cloud Function, only checking caller permission for now

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -7,9 +7,25 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.syncDatabaseOnUserCreation = functions.auth.user().onCreate((user) => {
-  return admin.database().ref("/users/" + user.uid + "/email").set(user.email);
+  return admin.database().ref('/users/' + user.uid + '/email').set(user.email);
 });
 
 exports.syncDatabaseOnUserDeletion = functions.auth.user().onDelete((user) => {
-  return admin.database().ref("/users/" + user.uid).remove();
+  return admin.database().ref('/users/' + user.uid).remove();
 });
+
+exports.addUserToEvent = functions.https.onCall((data, context) => {
+  // Extract parameters from client
+  const eventId = data.eventId;
+  const userEmail = data.userEmail;
+  const role = data.role;
+
+  // Auhtenticated user
+  const currentUserUid = context.auth.uid;
+  admin.database()
+    .ref('/events/' + eventId + '/users/admin/')
+    .once('value', snapshot => {
+      // TODO: find uid from email, insert into event
+      return snapshot.val().contains(currentUserUid);
+    })
+})
