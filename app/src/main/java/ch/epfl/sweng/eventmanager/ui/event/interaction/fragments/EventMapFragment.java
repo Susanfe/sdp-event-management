1b3514815f0a5376ec9e8a1c8734e3ba6bc80be5
stanislas jouven <stanislasjouven@gmail.com>
2018-11-21 15:50:13 +0100

    Delete a textView that was for test the map but no more useful and delete some text when clicking to localisation button

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventMapFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventMapFragment.java
@@ -202,7 +202,7 @@ public class EventMapFragment extends AbstractShowcaseFragment implements
     }
 
     private GoogleMap.OnMyLocationButtonClickListener onMyLocationButtonClickListener = () -> {
-        Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
