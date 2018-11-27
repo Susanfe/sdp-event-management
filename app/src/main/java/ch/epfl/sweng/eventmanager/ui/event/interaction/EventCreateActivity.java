22dc85edb7b75187d1df06d0331152e650b64b02
zyuiop <zyuiop@zyuiop.net>
2018-11-27 12:27:15 +0100

    Add an activity to create or edit an event

---
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventCreateActivity.java
@@ -0,0 +1,107 @@
+package ch.epfl.sweng.eventmanager.ui.event.interaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import javax.inject.Inject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProviders;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ch.epfl.sweng.eventmanager.R;
+import ch.epfl.sweng.eventmanager.repository.EventRepository;
+import ch.epfl.sweng.eventmanager.repository.data.Event;
+import ch.epfl.sweng.eventmanager.ui.event.interaction.fragments.schedule.ScheduleParentFragment;
+import ch.epfl.sweng.eventmanager.ui.event.interaction.fragments.user.EventUserManagementFragment;
+import ch.epfl.sweng.eventmanager.ui.event.interaction.models.EventInteractionModel;
+import ch.epfl.sweng.eventmanager.ui.event.selection.EventPickingActivity;
+import ch.epfl.sweng.eventmanager.viewmodel.ViewModelFactory;
+import dagger.android.AndroidInjection;
+
+public class EventCreateActivity extends AppCompatActivity {
+    private static final String TAG = "EventCreate";
+
+    @Inject
+    ViewModelFactory factory;
+
+    @Inject
+    EventRepository repository;
+
+    @BindView(R.id.create_form_send_button)
+    Button sendButton;
+    @BindView(R.id.create_form_name)
+    EditText name;
+    @BindView(R.id.create_form_email)
+    EditText email;
+    @BindView(R.id.create_form_twitter_handle)
+    EditText twitter;
+    @BindView(R.id.create_form_description)
+    EditText description;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+    @BindView(R.id.create_form)
+    View createForm;
+
+    private int eventID;
+    private Event event;
+
+    private void populateForm(Event event) {
+        this.name.setText(event.getName(), TextView.BufferType.EDITABLE);
+        this.email.setText(event.getOrganizerEmail(), TextView.BufferType.EDITABLE);
+        this.twitter.setText(event.getTwitterName(), TextView.BufferType.EDITABLE);
+        this.description.setText(event.getDescription(), TextView.BufferType.EDITABLE);
+    }
+
+    private void finishLoading() {
+        this.progressBar.setVisibility(View.GONE);
+        this.createForm.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_event_create);
+        ButterKnife.bind(this);
+
+        // Fetch event from passed ID
+        Intent intent = getIntent();
+        eventID = intent.getIntExtra(EventPickingActivity.SELECTED_EVENT_ID, -1);
+        if (eventID <= 0) {
+            // No event ID: we want to create an event
+            setTitle(R.string.create_event_title);
+            finishLoading();
+        } else {
+            LiveData<Event> event = repository.getEvent(eventID);
+
+            event.observe(this, ev -> {
+                if (ev == null) {
+                    Log.e(TAG, "Got null model from parent activity");
+                    return;
+                }
+
+                // Set window title and content
+                setTitle("Edit: " + ev.getName());
+                populateForm(ev);
+                finishLoading();
+
+                // Remove observer, because we don't want the form values to be overriden if the
+                // event changes remotely
+                // FIXME: or maybe we want that actually?
+                event.removeObservers(this);
+            });
+        }
+    }
+}
