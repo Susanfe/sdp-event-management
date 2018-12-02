8bf960390c7c47d4868194d7b0ac0974327b0b90
nicodarge <nicolas.dargenlieu@epfl.ch>
2018-12-02 18:21:43 +0100

    Set basics to get map

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventMapEditionFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventMapEditionFragment.java
@@ -1,10 +1,17 @@
 package ch.epfl.sweng.eventmanager.ui.event.interaction.fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.SupportMapFragment;
 
 import ch.epfl.sweng.eventmanager.R;
 
 public class EventMapEditionFragment extends AbstractShowcaseFragment {
+
+    private SupportMapFragment mapFragment;
 
     public EventMapEditionFragment() {
         super(R.layout.fragment_event_map_edition);
@@ -13,5 +20,16 @@ public class EventMapEditionFragment extends AbstractShowcaseFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.mapFragment);
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
