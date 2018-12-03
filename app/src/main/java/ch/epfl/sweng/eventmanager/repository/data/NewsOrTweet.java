c78d68f1527069db7a99fe313f5449bfebbaa8b3
stanislas jouven <stanislasjouven@gmail.com>
2018-12-03 21:46:09 +0100

    take into account the facebook news

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/NewsOrTweet.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/NewsOrTweet.java
@@ -14,21 +14,32 @@ import java.util.List;
 public class NewsOrTweet implements Comparable<NewsOrTweet> {
     public static final int TYPE_TWEET = 1;
     public static final int TYPE_NEWS = 2;
+    public static final int TYPE_FACEBOOK = 3;
 
     private final News news;
     private final Tweet tweet;
+    private final Feed facebook;
     private final long time;
 
     NewsOrTweet(News news) {
         this.news = news;
         this.tweet = null;
+        this.facebook = null;
         this.time = news.getDate();
     }
 
     NewsOrTweet(Tweet tweet) {
         this.news = null;
         this.tweet = tweet;
+        this.facebook = null;
         this.time = Date.parse(tweet.createdAt);
+    }
+
+    NewsOrTweet(Feed facebook) {
+        this.news = null;
+        this.tweet = null;
+        this.facebook = facebook;
+        this.time = facebook.getTime().getTime();
     }
 
     public News getNews() {
@@ -38,6 +49,8 @@ public class NewsOrTweet implements Comparable<NewsOrTweet> {
     public Tweet getTweet() {
         return tweet;
     }
+
+    public Feed getFacebook() {return facebook; }
 
     public long getTime() {
         return time;
@@ -49,10 +62,16 @@ public class NewsOrTweet implements Comparable<NewsOrTweet> {
     }
 
     public int getItemType() {
-        return this.tweet == null ? TYPE_NEWS : TYPE_TWEET;
+        if(this.tweet != null) {
+            return TYPE_TWEET;
+        }
+        else if(this.facebook != null) {
+            return TYPE_FACEBOOK;
+        }
+        return TYPE_NEWS;
     }
 
-    public static List<NewsOrTweet> mergeLists(List<News> news, List<Tweet> tweets) {
+    public static List<NewsOrTweet> mergeLists(List<News> news, List<Tweet> tweets, List<Feed> facebookNews) {
         List<NewsOrTweet> target = new ArrayList<>();
         if (news != null)
             for (News n : news)
@@ -61,6 +80,10 @@ public class NewsOrTweet implements Comparable<NewsOrTweet> {
         if (tweets != null)
             for (Tweet t : tweets)
                 target.add(new NewsOrTweet(t));
+
+        if (facebookNews != null)
+            for (Feed f : facebookNews)
+                target.add(new NewsOrTweet(f));
 
         return target;
     }
