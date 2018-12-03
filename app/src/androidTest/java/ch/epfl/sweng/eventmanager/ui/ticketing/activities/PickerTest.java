ee83cec94461b7f002a55f8dfb416ff5f991cc12
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2018-12-03 19:16:12 +0100

    api 21 seems to be better

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/ticketing/activities/PickerTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/ui/ticketing/activities/PickerTest.java
@@ -1,26 +1,21 @@
 package ch.epfl.sweng.eventmanager.ui.ticketing.activities;
 
 import android.os.SystemClock;
-
-import org.hamcrest.Matchers;
-import org.junit.Test;
-
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.matcher.IntentMatchers;
 import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.ui.ticketing.TicketingActivity;
 import ch.epfl.sweng.eventmanager.ui.ticketing.TicketingConfigurationPickerActivity;
 import ch.epfl.sweng.eventmanager.ui.ticketing.TicketingScanActivity;
+import org.hamcrest.Matchers;
+import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
 
 public class PickerTest extends ActivityTest<TicketingConfigurationPickerActivity> {
-
 
     public PickerTest() {
         super(2, TicketingConfigurationPickerActivity.class);
@@ -28,7 +23,7 @@ public class PickerTest extends ActivityTest<TicketingConfigurationPickerActivit
 
     @Test
     public void testDisplaysConfigs() {
-        SystemClock.sleep(200);
+        SystemClock.sleep(1000);
 
         onView(withId(R.id.recycler)).check(matches(Matchers.allOf(
                 hasDescendant(withText("Config1")),
