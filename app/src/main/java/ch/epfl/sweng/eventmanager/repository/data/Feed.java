eb7b734dbc94e348c0b576f7551f0388327d4150
stanislas jouven <stanislasjouven@gmail.com>
2018-12-03 21:43:46 +0100

    add Feed class that enables to store information of the JSON that come from the GraphResponse of facebook

---
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/Feed.java
@@ -0,0 +1,51 @@
+package ch.epfl.sweng.eventmanager.repository.data;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Feed {
+    private String id = "";
+    private String content = "";
+    private String url = "";
+    private String description = "";
+    private Date time;
+    private String author = "";
+
+    public Date getTime() {
+        return time;
+    }
+
+    public Feed(JSONObject object) {
+        try {
+            String dateStr = object.getString("created_time");
+
+            time = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(dateStr);
+            id = object.getString("id");
+            if (object.has("full_picture")) {
+                url = object.getString("full_picture");
+            }
+
+            if (object.has("description")) {
+                description = object.getString("description");
+            }
+
+            if (object.has("message")) {
+                content = object.getString("message");
+            }
+
+            if (object.has("name")) {
+                author = object.getString("name");
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) { 
+            e.printStackTrace();
+        }
+    }
+}
