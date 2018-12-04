753fd7afa5f16ef812405b5c7627c648ca7c1c83
nicodarge <nicolas.dargenlieu@epfl.ch>
2018-12-04 14:34:45 +0100

    Added different map style for edition

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventMapEditionFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventMapEditionFragment.java
@@ -1,17 +1,38 @@
 package ch.epfl.sweng.eventmanager.ui.event.interaction.fragments;
 
+import android.content.res.Resources;
+import android.util.Log;
+
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
+import java.util.Objects;
 
+import ch.epfl.sweng.eventmanager.R;
 import ch.epfl.sweng.eventmanager.repository.data.Spot;
 
 public class EventMapEditionFragment extends EventMapFragment implements GoogleMap.OnMarkerClickListener {
 
+    private static final String TAG = "MapEdition.STYLE_TAG";
+
     @Override
     protected void setUpCluster() {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            Objects.requireNonNull(getContext()), R.raw.map_edit_style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
         mMap.setOnMarkerClickListener(this);
         addMarkers();
     }
