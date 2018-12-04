75e9c6cbc5b10e8f567cfa3eb9c2639dcd84c806
stanislas jouven <stanislasjouven@gmail.com>
2018-12-04 16:45:42 +0100

    added the id to know the author of the post

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/Feed.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/Feed.java
@@ -1,6 +1,8 @@
 package ch.epfl.sweng.eventmanager.repository.data;
 
 
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,37 +10,41 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Feed {
     private String id = "";
     private String content = "";
-    private String url = "";
+    //private String url = "";
     private String description = "";
     private Date time;
-    private String author = "";
+    //private String author = "";
 
     public Feed(JSONObject object) {
         try {
             String dateStr = object.getString("created_time");
-
             time = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(dateStr);
             id = object.getString("id");
+            /*
             if (object.has("full_picture")) {
                 url = object.getString("full_picture");
             }
-
+            */
+            /*
             if (object.has("description")) {
                 description = object.getString("description");
             }
-
+            */
             if (object.has("message")) {
                 content = object.getString("message");
             }
-
+            /*
             if (object.has("name")) {
                 author = object.getString("name");
             }
+            */
         }
+
         catch (JSONException e) {
             e.printStackTrace();
         } catch (ParseException e) { 
@@ -60,6 +66,22 @@ public class Feed {
 
     public String getContent() {
         return content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Feed)) return false;
+
+        Feed that = (Feed) o;
+        if(!id.equals(that.id)) return false;
+        if(!time.equals(that.time)) return false;
+        if(!content.equals(that.content)) return false;
+        return true;
     }
 
 }
