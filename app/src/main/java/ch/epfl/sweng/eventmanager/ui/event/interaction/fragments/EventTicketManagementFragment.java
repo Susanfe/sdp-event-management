0efa946241d4d5c1bdc9054257f7312ba1578464
Timothée Floure <timothee.floure@fnux.ch>
2018-12-04 08:57:14 +0100

    Ticket management: design proper upload form and help text

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventTicketManagementFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventTicketManagementFragment.java
@@ -1,20 +1,38 @@
 package ch.epfl.sweng.eventmanager.ui.event.interaction.fragments;
 
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import ch.epfl.sweng.eventmanager.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Allows an administrator a manage the users allowed to work with this event (= access to admin
  * features).
  */
 public class EventTicketManagementFragment extends AbstractShowcaseFragment {
+    private final int ACTIVITY_CHOOSE_FILE = 1;
     private static final String TAG = "EventTicketManagement";
+
+    @BindView(R.id.select_file_button)
+    Button selectFileButton;
+
+    @BindView(R.id.file_path_field)
+    EditText filePathField;
+
+    @BindView(R.id.upload_button)
+    Button uploadButton;
 
     public EventTicketManagementFragment() {
         // Required empty public constructor
@@ -38,6 +56,31 @@ public class EventTicketManagementFragment extends AbstractShowcaseFragment {
         View view = super.onCreateView(inflater, container, savedInstanceState);
         ButterKnife.bind(this, view);
 
+        selectFileButton.setText(getString(R.string.select_file_button));
+        selectFileButton.setOnClickListener(v -> {
+            Intent chooseFile;
+            Intent intent;
+            chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+            chooseFile.setType("text/plain");
+            intent = Intent.createChooser(chooseFile, "Choose a file");
+            startActivityForResult(intent, ACTIVITY_CHOOSE_FILE);
+        });
+
+        uploadButton.setText(getString(R.string.upload_button));
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case ACTIVITY_CHOOSE_FILE: {
+                if (resultCode == RESULT_OK){
+                    Uri uri = data.getData();
+                    String filePath = uri.getPath();
+                    filePathField.setText(filePath);
+                }
+            }
+        }
     }
 }
