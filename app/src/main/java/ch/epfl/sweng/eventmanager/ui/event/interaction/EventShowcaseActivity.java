71c04ceff3be2d8a961b7e19ac78cb052c19c88e
Timothée Floure <timothee.floure@fnux.ch>
2018-12-04 09:14:01 +0100

    Remove unused "Tickets" activity from the event navigation

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventShowcaseActivity.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventShowcaseActivity.java
@@ -169,10 +169,6 @@ public class EventShowcaseActivity extends MultiFragmentActivity {
                 callChangeFragment(FragmentType.MAP, true);
                 break;
 
-            case R.id.nav_tickets:
-                callChangeFragment(null, true);
-                break;
-
             case R.id.nav_news:
                 callChangeFragment(FragmentType.NEWS, true);
                 break;
@@ -247,7 +243,6 @@ public class EventShowcaseActivity extends MultiFragmentActivity {
             if (current instanceof NewsFragment) navigationView.setCheckedItem(R.id.nav_news);
             if (current instanceof EventMapFragment) navigationView.setCheckedItem(R.id.nav_map);
             if (current instanceof ScheduleParentFragment) navigationView.setCheckedItem(R.id.nav_schedule);
-            if (current instanceof EventTicketFragment) navigationView.setCheckedItem(R.id.nav_tickets);
         });
     }
 
