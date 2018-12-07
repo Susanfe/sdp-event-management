1529a1b60d233aff531354e9466f2a2ef343d8f5
nicodarge <nicolas.dargenlieu@epfl.ch>
2018-12-07 16:38:15 +0100

    Implemented two types of actions : MoveMarkerAction and ModifyInfoAction

---
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/MapEditionData/ModifyMarkerInfoAction.java
@@ -0,0 +1,42 @@
+package ch.epfl.sweng.eventmanager.repository.data.MapEditionData;
+
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.List;
+
+public class ModifyMarkerInfoAction extends MapEditionAction {
+
+    private String ancientTitle;
+    private String title;
+
+    private String ancientSnippet;
+    private String snippet;
+
+    public ModifyMarkerInfoAction(MarkerType tag, String ancientTitle, String currentTitle,
+                                  String ancientSnippet, String currentSnippet) {
+
+        if (ancientTitle == null || currentTitle == null ||
+                ancientSnippet == null || currentSnippet == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.tag = tag;
+        this.title = currentTitle;
+        this.ancientTitle=ancientTitle;
+        this.snippet=currentSnippet;
+        this.ancientSnippet=ancientSnippet;
+
+    }
+
+    @Override
+    public boolean revert(List<Marker> markers) {
+        Marker marker = findMarkerByTag(markers);
+        if (marker == null || !marker.getTitle().equals(title) || !marker.getSnippet().equals(snippet))
+            return false;
+        else {
+            marker.setTitle(ancientTitle);
+            marker.setSnippet(ancientSnippet);
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/MapEditionData/MoveMarkerAction.java
@@ -0,0 +1,31 @@
+package ch.epfl.sweng.eventmanager.repository.data.MapEditionData;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.List;
+
+public class MoveMarkerAction extends MapEditionAction {
+
+    private final LatLng ancientPos;
+    private final LatLng pos;
+
+    public MoveMarkerAction(MarkerType markerTag, LatLng ancientPosition, LatLng actualPosition) {
+        if (markerTag == null || ancientPosition == null || actualPosition == null)
+            throw new IllegalArgumentException();
+
+        this.tag = markerTag;
+        this.ancientPos = ancientPosition;
+        this.pos = actualPosition;
+    }
+
+    @Override
+    public boolean revert(List<Marker> markers) {
+        Marker marker = findMarkerByTag(markers);
+        if (marker == null || !marker.getPosition().equals(pos)) return false;
+        else {
+            marker.setPosition(ancientPos);
+            return true;
+        }
+    }
+}
