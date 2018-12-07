0714c9f3d2be271c46f80c0b666ed6b780b5925e
nicodarge <nicolas.dargenlieu@epfl.ch>
2018-12-07 17:01:57 +0100

    Refactored markertype to include id

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/MapEditionData/MarkerType.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/MapEditionData/MarkerType.java
@@ -1,5 +1,16 @@
 package ch.epfl.sweng.eventmanager.repository.data.MapEditionData;
 
 public enum MarkerType {
-    SPOT, OVERLAY_EDGE
+    SPOT, OVERLAY_EDGE, TO_REMOVE;
+
+    int id = 0;
+
+    public MarkerType setId(int newId) {
+        id = newId;
+        return this;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
