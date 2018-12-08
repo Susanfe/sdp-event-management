043900f615156fe350f68cd1a93d13cb102daaeb
stanislas jouven <stanislasjouven@gmail.com>
2018-12-08 12:08:25 +0100

    use of string ressource instead of hardcoded string and go back to EventPickerActivity when ckicking on back button

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/user/LoginFacebookActivity.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/user/LoginFacebookActivity.java
@@ -2,20 +2,16 @@ package ch.epfl.sweng.eventmanager.ui.user;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import ch.epfl.sweng.eventmanager.R;
-import ch.epfl.sweng.eventmanager.ui.event.interaction.EventShowcaseActivity;
 import ch.epfl.sweng.eventmanager.ui.event.selection.EventPickingActivity;
 
 public class LoginFacebookActivity extends AppCompatActivity {
@@ -27,8 +23,6 @@ public class LoginFacebookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_facebook);
-
-
         loginButton = findViewById(R.id.loginButton);
 
         callbackManager = CallbackManager.Factory.create();
@@ -41,20 +35,17 @@ public class LoginFacebookActivity extends AppCompatActivity {
 
             @Override
             public void onCancel() {
-                //FIXME: use ressource
-                Toast.makeText(getApplicationContext(), "cancel login", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.cancel_login_facebook), Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(FacebookException error) {
-                //FIXME: use ressource
-                Toast.makeText(getApplicationContext(), "error login", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.error_login_facebook), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void changeScreen() {
-        //FIXME: which screen to go ?
         Intent intent = new Intent(this, EventPickingActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
@@ -64,6 +55,11 @@ public class LoginFacebookActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onBackPressed() {
+        changeScreen();
     }
 
 }
