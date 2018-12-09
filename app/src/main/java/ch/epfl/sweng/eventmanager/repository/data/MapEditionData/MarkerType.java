7dbff98ba4922efaedfdf21d41cb2fd5399dc1ad
nicodarge <nicolas.dargenlieu@epfl.ch>
2018-12-09 15:14:52 +0100

    Removed useless and errored methods and attributes from MarkerType enum

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/MapEditionData/MarkerType.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/MapEditionData/MarkerType.java
@@ -1,16 +1,5 @@
 package ch.epfl.sweng.eventmanager.repository.data.MapEditionData;
 
 public enum MarkerType {
-    SPOT, OVERLAY_EDGE, TO_REMOVE;
-
-    int id = 0;
-
-    public MarkerType setId(int newId) {
-        id = newId;
-        return this;
-    }
-
-    public int getId() {
-        return id;
-    }
+    SPOT, OVERLAY_EDGE, TO_REMOVE
 }
