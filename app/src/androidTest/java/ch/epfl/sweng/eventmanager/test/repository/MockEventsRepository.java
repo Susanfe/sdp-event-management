2437cda45c1493f9d6e8884525d395c63ea1cf21
AntoineCrettenand <lugiar13@gmail.com>
2018-12-12 19:45:13 +0100

    Implement dummy mock method

--- app/src/androidTest/java/ch/epfl/sweng/eventmanager/test/repository/MockEventsRepository.java
+++ app/src/androidTest/java/ch/epfl/sweng/eventmanager/test/repository/MockEventsRepository.java
@@ -3,6 +3,7 @@ package ch.epfl.sweng.eventmanager.test.repository;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import androidx.lifecycle.LiveData;
+import ch.epfl.sweng.eventmanager.notifications.NotificationRequest;
 import ch.epfl.sweng.eventmanager.repository.CloudFunction;
 import ch.epfl.sweng.eventmanager.repository.EventRepository;
 import ch.epfl.sweng.eventmanager.repository.data.*;
@@ -13,6 +14,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.yalantis.ucrop.view.OverlayView;
 
 import java.util.*;
 
@@ -239,6 +241,11 @@ public class MockEventsRepository implements EventRepository, CloudFunction {
 
         // TODO: we currently only check if this is properly called
 
+        return Tasks.call(() -> true);
+    }
+
+    @Override
+    public Task<Boolean> sendNotificationToUsers(NotificationRequest notificationRequest) {
         return Tasks.call(() -> true);
     }
 }
