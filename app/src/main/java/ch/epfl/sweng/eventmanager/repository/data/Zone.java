5206f012b211d7e7eb25422f7a6ce4d86151c63b
nicodarge <nicolas.dargenlieu@epfl.ch>
2018-12-12 23:19:40 +0100

    Computed correct location for additional point in Zone

--- app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/Zone.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/repository/data/Zone.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import com.google.android.gms.maps.model.PolygonOptions;
 import java.util.List;
 
+import androidx.core.math.MathUtils;
+
 /**
  * This class represents a delineated geographical zone that is overlayed by a blue color using the
  * addPolygon method.
@@ -16,8 +18,7 @@ public class Zone {
     private static final int OVERLAY_INSIDE_COLOR = Color.argb(33, 24, 149, 244);
 
     /**
-     * constructor
-     *
+     *  Creates a Zone from the initial positions as edges of the borderline
      * @param positions that delineated the zone
      */
     public Zone(List<Position> positions) {
@@ -25,7 +26,7 @@ public class Zone {
     }
 
     /**
-     * empty contructor needed for firebase
+     * Empty contructor needed for firebase
      */
     public Zone(){}
 
@@ -37,9 +38,10 @@ public class Zone {
                 .fillColor(OVERLAY_INSIDE_COLOR)
                 .strokeWidth(3)
                 .strokeColor(Color.BLUE);
-        for(int i = 0; i < positions.size(); ++i) {
-            options.add(positions.get(i).asLatLng());
-        }
+
+        for (Position p : positions)
+            options.add(p.asLatLng());
+
         return options;
     }
 
@@ -49,4 +51,63 @@ public class Zone {
     public List<Position> getPositions() {
         return positions;
     }
+
+    /**
+     * Adds a new position to the list of registered positions.
+     * @param position Position to add to zone as one of its edges
+     */
+    public void addPosition(Position position) {
+        if (position != null) {
+            int indexToPlace = computeIndexForPosition(position);
+            positions.add(indexToPlace, position);
+        } else throw new IllegalArgumentException("Trying to add null position!");
+    }
+
+    /**
+     * Computes the pair of consecutive points to which the given position is the nearest.
+     * @param position position to place in the array of positions.
+     * @return the index of the first of the pair in the middle of which to insert the position.
+     */
+    private int computeIndexForPosition(Position position) {
+        double min = Integer.MAX_VALUE;
+        int minIndex = 0;
+        int length = positions.size();
+        for (int i = 0; i < length; i++) {
+            double norm = computeDistance(position, positions.get(i%length),positions.get((i+1)%length));
+            if (norm < min) {
+                min = norm;
+                minIndex = i;
+            }
+        }
+
+        return (minIndex+1)%length;
+    }
+
+    /**
+     * Computes the sum of the distances of one point to two other points.
+     * @param pos the point to compute the distance to
+     * @param firstPos other first point
+     * @param secondPos other second point
+     * @return the sum of their distances
+     */
+    private double computeDistance(Position pos, Position firstPos, Position secondPos) {
+        double distance1 = norm(pos, firstPos);
+        double distance2 = norm(pos, secondPos);
+
+        return distance1+distance2;
+    }
+
+    /**
+     * Computes the norm between position (two-dimensional) vectors.
+     * @param p1 first point
+     * @param p2 second point
+     * @return distance between the two.
+     */
+    private double norm(Position p1, Position p2) {
+        return Math.sqrt(
+                (p1.getLongitude() - p2.getLongitude())*(p1.getLongitude() - p2.getLongitude()) +
+                        (p1.getLatitude() - p2.getLatitude())*(p1.getLatitude() - p2.getLatitude()));
+    }
+
+
 }
