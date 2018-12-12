4cb41dbb894befc789b50a58940892d64f264ede
AntoineCrettenand <lugiar13@gmail.com>
2018-12-12 18:56:58 +0100

    Doesn't publish notification if format of topic is invalid

--- app/src/main/java/ch/epfl/sweng/eventmanager/notifications/FirebaseNotificationService.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/notifications/FirebaseNotificationService.java
@@ -11,6 +11,9 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Custom FirebaseMessagingService handles received remote message from Firebase Cloud Messaging Service and alerts the
  * user with a notification.
@@ -26,30 +29,46 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
         Log.d(TAG, "Received a message : " + remoteMessage);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
+        // Check if the user has disabled notifications globally
         if (sharedPreferences.getBoolean(getApplicationContext().getString(R.string.key_settings_notifications_switch), true)) {
             RemoteMessage.Notification notif = remoteMessage.getNotification();
 
             if (notif != null) {
                 String targetTopics = remoteMessage.getFrom();
                 if (targetTopics != null) {
-                    Notification notification = NotificationBuilder.getFirebaseNotificationFrom(getApplicationContext(), notif.getTitle(), notif.getBody(), extractEventId(targetTopics));
-                    SchedulerHelper.scheduleNotification(getApplicationContext(), notif.hashCode(), notification, 0);
+                    Integer eventId = extractEventId(targetTopics);
+                    if (eventId != null) {
+                        Notification notification = NotificationBuilder.getFirebaseNotificationFrom(getApplicationContext(), notif.getTitle(), notif.getBody(), eventId);
+                        SchedulerHelper.scheduleNotification(getApplicationContext(), notif.hashCode(), notification, 0);
+                    }
                 }
             }
         }
     }
 
+    /**
+     * Subscribe the user to notifications specific to {@param ev}
+     */
     public static void subscribeToNotifications(Event ev) {
         FirebaseMessaging.getInstance().subscribeToTopic(getTopicName(ev));
     }
 
+    /**
+     * Unsubscribe the user to notifications specific to {@param ev}
+     */
     public static void unsubscribeFromNotifications(Event ev) {
         FirebaseMessaging.getInstance().unsubscribeFromTopic(getTopicName(ev));
     }
 
-    private static int extractEventId(String to) {
+    private static Integer extractEventId(String to) {
         String topic = to.replace("/topics/", "");
-        return Integer.parseInt(topic);
+        Integer result;
+        try {
+            result = Integer.decode(topic);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+        return result;
     }
 
     private static String getTopicName(Event ev) {
