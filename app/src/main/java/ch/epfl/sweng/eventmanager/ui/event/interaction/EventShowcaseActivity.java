f8f0f8fec7090fbec6f921cfd017c8a97ba9f817
stanislas jouven <stanislasjouven@gmail.com>
2018-12-15 23:16:06 +0100

    restore merge that was not successfully done

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventShowcaseActivity.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/EventShowcaseActivity.java
@@ -326,12 +326,6 @@ public class EventShowcaseActivity extends MultiFragmentActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_facebook_login, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_facebook_login_edit:
@@ -355,6 +349,7 @@ public class EventShowcaseActivity extends MultiFragmentActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_facebook_login, menu);
         getMenuInflater().inflate(R.menu.menu_showcase_activity_join, menu);
         Switch s = (Switch) menu.findItem(R.id.menu_showcase_activity_join_id).getActionView();
         model.getEvent().observe(this, ev -> {
@@ -397,16 +392,6 @@ public class EventShowcaseActivity extends MultiFragmentActivity {
         toast.show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
-            default:
-                return false;
-        }
-    }
 
     public enum FragmentType {
         MAIN, MAP, SCHEDULE, NEWS, FORM
