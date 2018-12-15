90e5df1eaba5b5c7e430abfad6a17c937424fd54
AntoineCrettenand <lugiar13@gmail.com>
2018-12-15 11:44:05 +0100

    Fix issue with submitting feedback button

--- app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventFeedbackFragment.java
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/event/interaction/fragments/EventFeedbackFragment.java
@@ -7,23 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ch.epfl.sweng.eventmanager.R;
-import ch.epfl.sweng.eventmanager.repository.FeedbackRepository;
-import ch.epfl.sweng.eventmanager.repository.data.EventRating;
 import ch.epfl.sweng.eventmanager.ui.event.interaction.EventShowcaseActivity;
 import dagger.android.support.AndroidSupportInjection;
 
-import javax.inject.Inject;
 import java.util.Collections;
-import java.util.Date;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class EventFeedbackFragment extends AbstractFeedbackFragment {
@@ -67,24 +62,26 @@ public class EventFeedbackFragment extends AbstractFeedbackFragment {
 
             if (ratingExists.get() != null && ratingExists.get()) {
                 submitFeedback.setVisibility(View.GONE);
-            }
+            } else submitFeedback.setVisibility(View.VISIBLE);
 
             repository.getRatings(ev.getId()).observe(this, ratings -> {
                 if (ratings != null && ratings.size() > 0) {
                     recyclerView.setVisibility(View.VISIBLE);
                     emptyRatingsTextView.setVisibility(View.GONE);
                     ratingsRecyclerViewAdapter.setContent(ratings);
-                    submitFeedback.setVisibility(View.GONE);
                 } else {
                     recyclerView.setVisibility(View.GONE);
                     submitFeedback.setVisibility(View.VISIBLE);
                     ratingsRecyclerViewAdapter.setContent(Collections.emptyList());
-                    emptyRatingsTextView.setVisibility(View.VISIBLE);
                 }
             });
-        });
 
-        submitFeedback.setOnClickListener(l -> ((EventShowcaseActivity) getActivity()).changeFragment(new SubmitFeedbackFragment(), true));
+            submitFeedback.setOnClickListener(l -> {
+                if (ratingExists.get() != null && !ratingExists.get())
+                    ((EventShowcaseActivity) getActivity()).changeFragment(new SubmitFeedbackFragment(), true);
+                else Toast.makeText(getActivity(), R.string.event_feedback_already_submitted, Toast.LENGTH_SHORT).show();
+            });
+        });
     }
 
     @Override
