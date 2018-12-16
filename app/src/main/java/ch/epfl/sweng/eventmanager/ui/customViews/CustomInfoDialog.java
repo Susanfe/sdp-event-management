87b349777e7ba064748d5ba5c773d6c796ca53df
nicodarge <nicolas.dargenlieu@epfl.ch>
2018-12-16 01:50:41 +0100

    Implemented info dialog

---
+++ app/src/main/java/ch/epfl/sweng/eventmanager/ui/customViews/CustomInfoDialog.java
@@ -0,0 +1,47 @@
+package ch.epfl.sweng.eventmanager.ui.customViews;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import ch.epfl.sweng.eventmanager.R;
+
+public class CustomInfoDialog extends DialogFragment {
+
+    private static final String CUSTOM_DIALOG_INFO_STRING = "ui.customViews.CUSTOM_DIALOG_INFO_STRING";
+    private static final String DEFAULT_INFO = "No info";
+    private String info;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                Objects.requireNonNull(getActivity()),
+                R.style.CustomDialog_TransparentBackground);
+
+        if (info == null)
+            builder.setMessage(DEFAULT_INFO);
+        else
+            builder.setMessage(info);
+
+        // Quit dialog on OK button click
+        builder.setNeutralButton(R.string.ok, (dialog, which) -> {});
+
+        return builder.create();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Bundle bundle = getArguments();
+        assert bundle!=null;
+
+        info = bundle.getString(CUSTOM_DIALOG_INFO_STRING);
+    }
+}
